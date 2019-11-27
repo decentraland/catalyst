74d1caea54bd9f67bed8cbd056194e6bcd6c2d97
Pablo de Haro <pablitar@gmail.com>
2019-11-27 11:33:21 -0300

    A couple of commands for the server

--- lighthouse/server.ts
+++ lighthouse/server.ts
@@ -1,13 +1,15 @@
-import cors from 'cors'
-import express from 'express'
+import cors from "cors";
+import express from "express";
 
-const port = process.env.PORT ?? 9000
+const port = process.env.PORT ?? 9000;
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 app.get("/hello", (req, res, next) => {
-    res.send("Hello world!!!")
-})
+  res.send("Hello world!!!");
+});
 
-app.listen(port)
+app.listen(port, () => {
+  console.info(`==> Lighthouse listening on port ${port}.`);
+});
