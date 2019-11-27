c75a5c00963387bf4ab54912e9afe52ae751b613
moliva <oliva.miguelh@gmail.com>
2019-11-27 18:04:33 -0300

    chore: adding peerjs server

--- peer/Peer.ts
+++ peer/Peer.ts
@@ -4,14 +4,12 @@ export class Peer implements IPeer {
   private peer: PeerJS;
 
   constructor(private lighthouseUrl: string, public nickname: string) {
-    this.peer = new PeerJS(
-      nickname
-      //   {
-      //   host: "localhost",
-      //   port: 9000,
-      //   path: "/"
-      // }
-    );
+    // TODO - change peer js server to use actual lighthouse url - moliva - 27/11/2019
+    this.peer = new PeerJS(nickname, {
+      host: "localhost",
+      port: 9000,
+      path: "/"
+    });
     this.peer.on("connection", conn => {
       conn.on("data", data => {
         console.log(data);
@@ -32,19 +30,21 @@ export class Peer implements IPeer {
     }).then(res => res.json());
     console.log(room);
 
-    room.forEach(user => {
-      const conn = this.peer.connect(user.id);
+    room
+      .filter(user => user.id !== this.nickname)
+      .forEach(user => {
+        const conn = this.peer.connect(user.id);
 
-      console.log(`opening connection to ${user.id}`);
-      conn.on("open", () => {
-        console.log(`connection open hi to ${user.id}`);
-        conn.send(`hi, I'm ${this.nickname}!`);
+        console.log(`opening connection to ${user.id}`);
+        conn.on("open", () => {
+          console.log(`connection open hi to ${user.id}`);
+          conn.send(`hi, I'm ${this.nickname}!`);
+        });
       });
-    });
   }
 }
 
 export interface IPeer {
-  nickname: string
-  joinRoom(room: string): Promise<void>
+  nickname: string;
+  joinRoom(room: string): Promise<void>;
 }
