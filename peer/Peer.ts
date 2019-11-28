6d11bd87f91032d56f2b4ee153a05e29ba962c50
moliva <oliva.miguelh@gmail.com>
2019-11-28 11:13:24 -0300

    adding messages to chat

--- peer/Peer.ts
+++ peer/Peer.ts
@@ -1,7 +1,21 @@
 import PeerJS from "peerjs";
 
+export type User = { id: string };
+export type Room = { id: string; users: User[] };
+
+export interface IPeer {
+  nickname: string;
+  currentRooms: Room[];
+  joinRoom(room: string): Promise<void>;
+  sendMessage(room: string, message: string): Promise<void>;
+}
+
+type PeerData = { id: string; connection: any };
+
 export class Peer implements IPeer {
   private peer: PeerJS;
+  private peers: PeerData[] = [];
+
   public readonly currentRooms: Room[] = [];
 
   constructor(private lighthouseUrl: string, public nickname: string) {
@@ -11,9 +25,20 @@ export class Peer implements IPeer {
       port: 9000,
       path: "/"
     });
+
     this.peer.on("connection", conn => {
+      console.log(
+        `connection received from ${conn.peer}, in channel ${conn.label}`
+      );
+      this.peers.push({ id: conn.peer, connection: conn });
+
       conn.on("data", data => {
         console.log(data);
+      });
+
+      conn.on("close", () => {
+        // remove peer from channels and peer list
+        // const room = this.currentRooms[conn.label];
       });
     });
   }
@@ -32,26 +57,17 @@ export class Peer implements IPeer {
     room
       .filter(user => user.id !== this.nickname)
       .forEach(user => {
-        const conn = this.peer.connect(user.id);
+        const conn = this.peer.connect(user.id, { label: roomId });
 
         conn.on("open", () => {
-          console.log(`connection open hi to ${user.id}`);
-          conn.send(`hi, I'm ${this.nickname}!`);
+          console.log(`connection open to ${user.id}`);
+          this.peers.push({ id: user.id, connection: conn });
+          // conn.send(`hi, I'm ${this.nickname}!`);
         });
       });
   }
 
   sendMessage(roomId: string, message: string) {
-    return Promise.resolve()
+    return Promise.resolve();
   }
-}
-
-export type User = { id: string }
-export type Room = { id: string; users: User[] };
-
-export interface IPeer {
-  nickname: string;
-  currentRooms: Room[];
-  joinRoom(room: string): Promise<void>;
-  sendMessage(room: string, message: string): Promise<void>;
 }
