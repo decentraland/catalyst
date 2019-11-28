37a36b2c3b22b47303909bd85b66354565837a1e
Pablo de Haro <pablitar@gmail.com>
2019-11-28 17:55:45 -0300

    Syncing cursor positions

--- peer-react-app/components/Chat.tsx
+++ peer-react-app/components/Chat.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { IPeer } from "../../peer/Peer";
-import { Button } from "decentraland-ui";
+import { Button, Radio } from "decentraland-ui";
 
 type Message = {
   sender: string;
@@ -23,6 +23,38 @@ function MessageBubble(props: { message: Message; own?: boolean }) {
   );
 }
 
+function CursorComponent(props: {cursor: Cursor}) {
+  return <div className="other-cursor" style={
+      {
+        left: props.cursor.x + "px",
+        top: props.cursor.y + "px", 
+        backgroundColor: props.cursor.color
+      }
+  } />
+}
+
+const mouse = {
+  x: 0,
+  y: 0
+}
+
+type Cursor = {
+  x: number;
+  y: number;
+  color: string;
+}
+
+const mouseListener = (ev: MouseEvent) => {
+  mouse.x = ev.pageX
+  mouse.y = ev.pageY
+}
+
+function randomColor() {
+  return "hsl(" + Math.floor(Math.random() * 359) + ", 100%, 50%)";
+}
+
+let intervalId: number | undefined = undefined
+
 export function Chat(props: { peer: IPeer; room: string }) {
   //@ts-ignore
   const [messages, setMessages] = useState([
@@ -31,6 +63,8 @@ export function Chat(props: { peer: IPeer; room: string }) {
   ] as Message[]);
 
   const [message, setMessage] = useState("");
+  const [cursors, setCursors] = useState<Record<string, Cursor>>({})
+  const [updatingCursors, setUpdatingCursors] = useState(false)
   const messagesEndRef: any = useRef();
 
   props.peer.callback = (sender, room, payload) => {
@@ -41,10 +75,33 @@ export function Chat(props: { peer: IPeer; room: string }) {
       case "chat":
         appendMessage(sender, payload.message);
         break;
+      case "cursorPosition":
+        setCursorPosition(sender, payload.position)
+        break;
       default:
-        console.log("Received unknown message type: " + payload.type)
+          console.log("Received unknown message type: " + payload.type)
     }
   };
+
+  function setCursorPosition(sender: string, position: {x: number, y: number}) {
+    if(updatingCursors){
+      const cursorColor = cursors[sender]?.color ?? randomColor()
+
+      setCursors({
+        ...cursors,
+        [sender]: {color: cursorColor, x: position.x, y: position.y }
+      })
+    }
+  }
+
+  function sendCursorMessage() {
+    console.log({ type: "cursorPosition", position: { ...mouse } })
+    props.peer.sendMessage(
+      props.room,
+      { type: "cursorPosition", position: { ...mouse } },
+      false
+    );
+  }
 
   function sendMessage() {
     appendMessage(props.peer.nickname, message);
@@ -60,11 +117,34 @@ export function Chat(props: { peer: IPeer; room: string }) {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   });
 
+  useEffect(() => {
+    document.addEventListener("mousemove", mouseListener)
+
+    return () => document.removeEventListener("mousemove", mouseListener)
+  }, [])
+
+  useEffect(() => {
+    window.clearInterval(intervalId);
+    if(updatingCursors) {
+      intervalId = window.setInterval(
+        () => sendCursorMessage()
+      , 100)
+    }
+
+    return () => window.clearInterval(intervalId)
+  }, [updatingCursors]);
+
   return (
     <div className="chat">
       <h2>Welcome to the Chat {props.peer.nickname}</h2>
       <div className="room-title">
         <h3>{props.room}</h3>
+        <Radio
+          toggle
+          label="Update cursors"
+          checked={updatingCursors}
+          onChange={(ev, data) => setUpdatingCursors(!!data.checked)}
+        />
       </div>
       <div className="messages-container">
         {messages.map((it, i) => (
@@ -82,7 +162,7 @@ export function Chat(props: { peer: IPeer; room: string }) {
       <div className="message-container">
         <textarea
           value={message}
-          onChange={ev => setMessage(ev.currentTarget.value)}
+          onChange={  ev => setMessage(ev.currentTarget.value)}
           onKeyDown={ev => {
             if (message && ev.keyCode === 13 && ev.ctrlKey) sendMessage();
           }}
@@ -96,6 +176,7 @@ export function Chat(props: { peer: IPeer; room: string }) {
           Send
         </Button>
       </div>
+      {updatingCursors && Object.values(cursors).map(it => <CursorComponent cursor={it} />)}
     </div>
   );
 }
