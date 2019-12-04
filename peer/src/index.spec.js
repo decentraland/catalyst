3ef34c344598c8735a2a0c3e89c0b764367c439f
moliva <oliva.miguelh@gmail.com>
2019-12-04 10:57:31 -0300

    chore: minimized changes

--- peer/src/index.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import { describe, it } from "mocha";
-
-describe("get performance info", function() {
-  it(`works correctly`, () => {
-    // expect(1).to.be.equalTo(1);
-  });
-});
--- peer/test/index.ts
+++ peer/test/index.ts
@@ -1,13 +1,5 @@
-// import { describe, it } from "mocha";
-// import { expect } from "chai";
-// import { expect } from "jasmine";
-
-describe("get performance info", function() {
+describe("tests in jasmine running on karma", function() {
   it(`works correctly`, () => {
-    // expect(1).to.be.equal(1);
-    // expect(false).to.be.equal(false);
-
-    // throw new Error("asdf");
-    // console.log("holis");
+    expect(true).toBe(true);
   });
 });
