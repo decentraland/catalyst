c05ad486152281502b597186319322e69e896764
moliva <oliva.miguelh@gmail.com>
2019-12-06 10:32:46 -0300

    feat: changing peerjs for simple peer impl

--- peer-react-app/static/config.js
+++ peer-react-app/static/config.js
@@ -1,18 +1,20 @@
 require.config({
-    paths: {
-      react: '/npm/node_modules/react/umd/react.development',
-      'react-dom': '/npm/node_modules/react-dom/umd/react-dom.development',
-      'peerjs': '/npm/node_modules/peerjs/dist/peerjs',
-      //'react-router': '/npm/node_modules/react-router/umd/react-router',
-      'decentraland-ui': '/npm/node_modules/decentraland-ui/lib/index',
-      'fp-future': 'npm/node_modules/fp-future/index'
-      //redux: '/npm/node_modules/redux/dist/redux',
-      //d3: '/npm/node_modules/d3/dist/d3',
-      //jsonwebtoken: 'static/jsonwebtoken.umd',
-      //'redux-saga': '/npm/node_modules/redux-saga/dist/redux-saga.umd',
-      //'redux-saga/effects': '/npm/node_modules/redux-saga/dist/redux-saga-effects.umd',
-      //'ethereum-cryptography/sha256': '/static/ethereum-cryptography/sha256.umd',
-      //'ethereum-cryptography/random': '/static/ethereum-cryptography/random.umd',
-      //'ethereum-cryptography/secp256k1': '/static/ethereum-cryptography/secp256k1.umd'
-    }
-  })
+  paths: {
+    react: "/npm/node_modules/react/umd/react.development",
+    "react-dom": "/npm/node_modules/react-dom/umd/react-dom.development",
+    peerjs: "/npm/node_modules/peerjs/dist/peerjs",
+    //'react-router': '/npm/node_modules/react-router/umd/react-router',
+    "decentraland-ui": "/npm/node_modules/decentraland-ui/lib/index",
+    "fp-future": "npm/node_modules/fp-future/index",
+    "simple-peer": "npm/node_modules/simple-peer/simplepeer.min",
+    // ws: "npm/node_modules/ws/index"
+    //redux: '/npm/node_modules/redux/dist/redux',
+    //d3: '/npm/node_modules/d3/dist/d3',
+    //jsonwebtoken: 'static/jsonwebtoken.umd',
+    //'redux-saga': '/npm/node_modules/redux-saga/dist/redux-saga.umd',
+    //'redux-saga/effects': '/npm/node_modules/redux-saga/dist/redux-saga-effects.umd',
+    //'ethereum-cryptography/sha256': '/static/ethereum-cryptography/sha256.umd',
+    //'ethereum-cryptography/random': '/static/ethereum-cryptography/random.umd',
+    //'ethereum-cryptography/secp256k1': '/static/ethereum-cryptography/secp256k1.umd'
+  }
+});
--- peer/src/Peer.ts
+++ peer/src/Peer.ts
@@ -1,4 +1,5 @@
-import PeerJS from "peerjs";
+import SimplePeer from "simple-peer";
+// @ts-ignore
 import { future, IFuture } from "fp-future";
 
 export type User = string;
@@ -14,20 +15,23 @@ export interface IPeer {
 
 type PeerData = {
   id: string;
-  reliableConnection: IFuture<PeerJS.DataConnection>;
-  unreliableConnection: IFuture<PeerJS.DataConnection>;
+  // reliableConnection: IFuture<SimplePeer.DataConnection>;
+  // unreliableConnection: IFuture<PeerJS.DataConnection>;
 };
 
 export class Peer implements IPeer {
-  private peer: PeerJS;
+  // private peer: SimplePeer.SimplePeer;
   private peers: Record<string, PeerData> = {};
+
+  private peerServer: WebSocket;
+  private connections: Record<string, SimplePeer.Instance> = {};
 
   public readonly currentRooms: Room[] = [];
 
   constructor(
     private lighthouseUrl: string,
     public nickname: string,
-    config: any = {},
+    private config: any = {},
     public callback: (
       sender: string,
       room: string,
@@ -35,44 +39,73 @@ export class Peer implements IPeer {
     ) => void = () => {}
   ) {
     const url = new URL(lighthouseUrl);
-    this.peer = new PeerJS(nickname, {
+    //@ts-ignore
+    const { host, port, path } = {
       host: url.hostname,
-      port: url.port ? parseInt(url.port) : 80,
-      path: url.pathname,
-      config
-    });
+      port: url.port ? parseInt(url.port) : 80
+      // path: url.pathname
+    };
 
-    this.peer.on("connection", conn => {
-      if (!this.peers[conn.peer]) {
-        this.peers[conn.peer] = {
-          id: conn.peer,
-          reliableConnection: future(),
-          unreliableConnection: future()
-        };
-      }
-      const reliable = conn.reliable
-        ? "reliableConnection"
-        : "unreliableConnection";
-      this.peers[conn.peer][reliable].resolve(conn);
+    console.log("about to create ws");
+    const ws = new WebSocket(
+      `ws://${host}:${port}/peerjs?key=peerjs&id=${this.nickname}&token=asdf`
+    );
 
-      this.currentRooms
-        .find(room => room.id === conn.label)
-        ?.users.add(conn.peer);
+    const self = this;
+    ws.onopen = function(event) {
+      console.log("ws open");
+      self.peerServer = ws;
+      setInterval(() => {
+        console.log("ws sending hearbeat");
+        ws.send(JSON.stringify({ type: "HEARBEAT" }));
+      }, 1000);
 
-      this.subscribeToConnection(conn);
+      ws.onmessage = function(e) {
+        const { data } = e;
+        console.log(`ws.onmessage ${JSON.stringify(data)}`);
+        if (data.type === "ANSWER" || data.type === "CANDIDATE") {
+          self.connections[data.payload.connectionId].signal(data.payload.sdp);
+        }
+      };
+    };
+    // this.peer = new SimplePeer(nickname, {
+    //   host: url.hostname,
+    //   port: url.port ? parseInt(url.port) : 80,
+    //   path: url.pathname,
+    //   config
+    // });
 
-      conn.on("close", () => {
-        // remove peer from channels and peer list
-        // const room = this.currentRooms[conn.label];
-      });
-    });
+    // this.peer.on("connection", conn => {
+    //   if (!this.peers[conn.peer]) {
+    //     this.peers[conn.peer] = {
+    //       id: conn.peer,
+    //       reliableConnection: future(),
+    //       unreliableConnection: future()
+    //     };
+    //   }
+    //   const reliable = conn.reliable
+    //     ? "reliableConnection"
+    //     : "unreliableConnection";
+    //   this.peers[conn.peer][reliable].resolve(conn);
+
+    //   this.currentRooms
+    //     .find(room => room.id === conn.label)
+    //     ?.users.add(conn.peer);
+
+    //   this.subscribeToConnection(conn);
+
+    //   conn.on("close", () => {
+    //     // remove peer from channels and peer list
+    //     // const room = this.currentRooms[conn.label];
+    //   });
+    // });
   }
 
-  private subscribeToConnection(conn: PeerJS.DataConnection) {
-    conn.on("data", data => {
-      this.callback(conn.peer, data.room, data.payload);
-    });
-  }
+  // private subscribeToConnection(conn: PeerJS.DataConnection) {
+  //   conn.on("data", data => {
+  //     this.callback(conn.peer, data.room, data.payload);
+  //   });
+  // }
 
   async joinRoom(roomId: string): Promise<void> {
     const room: { id: string }[] = await fetch(
@@ -96,28 +129,82 @@ export class Peer implements IPeer {
       .forEach(user => {
         const peer = {
           id: user.id,
-          reliableConnection: future(),
-          unreliableConnection: future()
+          reliableConnection: future()
+          // unreliableConnection: future()
         };
         this.peers[user.id] = peer;
         this.initConnection(roomId, peer, true);
-        this.initConnection(roomId, peer, false);
+        // this.initConnection(roomId, peer, false);
       });
   }
 
   private initConnection(roomId: string, peer: PeerData, reliable: boolean) {
-    const conn = this.peer.connect(peer.id, {
-      label: roomId,
-      reliable
+    // const conn = this.peer.connect(peer.id, {
+    //   label: roomId,
+    //   reliable
+    // });
+
+    // conn.on("open", () => {
+    //   this.subscribeToConnection(conn);
+    //   const connKey = reliable ? "reliableConnection" : "unreliableConnection";
+    //   peer[connKey].resolve(conn);
+    // });
+    console.log(`init connection to ${roomId}`);
+    const conn = new SimplePeer({
+      initiator: true,
+      trickle: true,
+      config: this.config,
+      channelName: "nestor"
     });
 
-    conn.on("open", () => {
-      this.subscribeToConnection(conn);
+    conn.on("signal", data => {
+      console.log(`signaling! ${JSON.stringify(data)}`);
+
+      // if (data.type == "offer") {
+      const offer = {
+        type: "OFFER",
+        src: this.nickname,
+        dst: peer.id,
+        payload: {
+          sdp: data,
+          type: "data",
+          connectionId: `dc_${this.create_UUID()}`,
+          browser: "chrome",
+          label: roomId,
+          reliable: reliable,
+          serialization: "binary"
+        }
+      };
+      this.connections[offer.payload.connectionId] = conn;
+      // conn.signal(data)
+      this.peerServer.send(JSON.stringify(offer));
+      // }
+    });
+
+    conn.on("connect", () => {
+      // wait for 'connect' event before using the data channel
+      console.log(`connected to: ${peer.id}`);
+      conn.send("hey peer2, how is it going?");
+
+      conn.on("data", data => {
+        console.log(`got data: ${JSON.stringify(data)}`);
+        this.callback("osvaldo", "room", data);
+      });
+
       const connKey = reliable ? "reliableConnection" : "unreliableConnection";
       peer[connKey].resolve(conn);
     });
   }
 
+  create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = "xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+      var r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+    });
+    return uuid;
+  }
   sendMessage(roomId: string, payload: any, reliable: boolean = true) {
     const room = this.currentRooms.find(room => room.id === roomId);
     if (!room) {
