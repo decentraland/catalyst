0c65453fd399eecd86ef39c27c9543ca32fde849
pablitar <pablitar@gmail.com>
2019-12-06 11:12:21 -0300

    Simple peer attempt (#4)

--- peer-react-app/static/config.js
+++ peer-react-app/static/config.js
@@ -5,7 +5,9 @@ require.config({
       'peerjs': '/npm/node_modules/peerjs/dist/peerjs',
       //'react-router': '/npm/node_modules/react-router/umd/react-router',
       'decentraland-ui': '/npm/node_modules/decentraland-ui/lib/index',
-      'fp-future': 'npm/node_modules/fp-future/index'
+      'fp-future': 'npm/node_modules/fp-future/index',
+      'simple-peer': 'npm/node_modules/simple-peer/simplepeer.min',
+      'eventemitter3': 'npm/node_modules/eventemitter3/umd/eventemitter3',
       //redux: '/npm/node_modules/redux/dist/redux',
       //d3: '/npm/node_modules/d3/dist/d3',
       //jsonwebtoken: 'static/jsonwebtoken.umd',
---
+++ peer/src/peerjs-server-connector/util.ts
@@ -0,0 +1,53 @@
+// const DEFAULT_CONFIG = {
+//   iceServers: [
+//     { urls: "stun:stun.l.google.com:19302" },
+//     {
+//       urls: "turn:0.peerjs.com:3478",
+//       username: "peerjs",
+//       credential: "peerjsp"
+//     }
+//   ],
+//   sdpSemantics: "unified-plan"
+// };
+
+export const util = new (class {
+  noop(): void {}
+
+  readonly CLOUD_HOST = "0.peerjs.com";
+  readonly CLOUD_PORT = 443;
+
+  // Ensure alphanumeric ids
+  validateId(id: string): boolean {
+    // Allow empty ids
+    return !id || /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/.test(id);
+  }
+
+  randomToken(): string {
+    return Math.random()
+      .toString(36)
+      .substr(2);
+  }
+
+  isSecure(): boolean {
+    return location.protocol === "https:";
+  }
+})();
+
+export const ConnectionSuffixes = {
+  reliable: "reliable",
+  unreliable: "unreliable"
+};
+
+export function connectionIdFor(
+  myId: string,
+  peerId: string,
+  reliable: boolean
+) {
+  return `${myId}_${peerId}_${
+    reliable ? ConnectionSuffixes.reliable : ConnectionSuffixes.unreliable
+  }`;
+}
+
+export function isReliable(connectionId: string) {
+  return !connectionId.endsWith(ConnectionSuffixes.unreliable)
+}
