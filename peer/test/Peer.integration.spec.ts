c14a70d29965953b8f16faeaaa6cddc79efb1561
Pablo de Haro <pablitar@gmail.com>
2019-12-11 17:02:47 -0300

    Replaced peerserverconnection mock with socket mock

--- peer/test/Peer.integration.spec.ts
+++ peer/test/Peer.integration.spec.ts
@@ -1,38 +1,65 @@
 import { Peer, PeerConnectionData } from "../src/Peer";
-import * as PeerConnectionModule from "../src/peerjs-server-connector/peerjsserverconnection";
-import { MockManager, ImportMock } from "ts-mock-imports";
 import { delay } from "../src/peerjs-server-connector/util";
+import { SocketType } from "../src/peerjs-server-connector/socket";
 
 const oldFetch = fetch;
 const globalScope: any = typeof window === "undefined" ? global : window;
 
+class SocketMock implements SocketType {
+  onmessage: any = () => {};
+  onclose: any = () => {};
+
+  set onopen(f: any) {
+    f();
+  }
+
+  readyState: number = 1;
+
+  constructor(private destination?: SocketMock) {
+    if (destination) {
+      destination.destination = this;
+    }
+  }
+
+  close(code?: number, reason?: string): void {}
+
+  send(
+    data: string | ArrayBuffer | SharedArrayBuffer | Blob | ArrayBufferView
+  ): void {
+    this.destination?.onmessage({ data });
+  }
+}
+
+const messageHandler: (sender: string, room: string, payload: any) => void = (
+  sender,
+  room,
+  payload
+) => {
+  console.log(`Received message from ${sender} in ${room}`, payload);
+};
+
 describe("Peer Integration Test", function() {
-  let connectionMock: MockManager<PeerConnectionModule.PeerJSServerConnection>;
   const peerIds: PeerConnectionData[] = [];
 
   beforeEach(() => {
     globalScope.fetch = (input, init) =>
       Promise.resolve(new Response(JSON.stringify(peerIds)));
-
-    connectionMock = ImportMock.mockClass(
-      PeerConnectionModule,
-      "PeerJSServerConnection"
-    );
-
-    // connectionMock.getMockInstance().sendOffer = (userId: String, offer) =>
   });
 
   afterEach(() => {
-    connectionMock.restore();
     globalScope.fetch = oldFetch;
   });
 
   it(`Performs handshake as expected`, async () => {
+    const peer1Socket = new SocketMock();
+    const peer2Socket = new SocketMock(peer1Socket);
+
     const peer1 = new Peer(
       "http://notimportant:8888/",
       "peer1",
-      (sender, room, payload) => {
-        console.log(`Received message from ${sender} in ${room}`, payload);
+      messageHandler,
+      {
+        socketBuilder: url => peer1Socket
       }
     );
 
@@ -43,63 +70,24 @@ describe("Peer Integration Test", function() {
     const peer2 = new Peer(
       "http://notimportant:8888/",
       "peer2",
-      (sender, room, payload) => {
-        console.log(`Received message from ${sender} in ${room}`, payload);
-      }
-    );
-
-    connectionMock.set(
-      "sendOffer",
-      (userId: string, offerData: any, connectionId: string) => {
-        peer1.handleMessage(
-          PeerConnectionModule.createOfferMessage(
-            "peer2",
-            userId,
-            offerData,
-            connectionId
-          )
-        );
-      }
-    );
-
-    connectionMock.set(
-      "sendAnswer",
-      (userId: string, answerData: any, connectionId: string) => {
-        peer2.handleMessage(
-          PeerConnectionModule.createAnswerMessage(
-            "peer1",
-            userId,
-            answerData,
-            connectionId
-          )
-        );
-      }
-    );
-
-    connectionMock.set(
-      "sendCandidate",
-      (userId: string, candidateData: any, connectionId: string) => {
-        peer2.handleMessage(
-          PeerConnectionModule.createCandidateMessage(
-            "peer1",
-            userId,
-            candidateData,
-            connectionId
-          )
-        );
+      messageHandler,
+      {
+        socketBuilder: url => peer2Socket
       }
     );
 
     await peer2.joinRoom("room");
 
-    await delay(2000);
+    //TODO: This delay should be replaced with a kind of promise eventually
+    await delay(600);
 
     const peer1Room = peer1.currentRooms[0];
     expect(peer1Room.id).toBe("room");
     expect(peer1Room.users.size).toBe(2);
     expect(peer1Room.users.has("peer2:peer2")).toBeTrue();
     //@ts-ignore
-    // const peer12peer2
-    // expect(peer1.peers["peer2"].reliableConnection).toBeDefined()
+    const peer1ToPeer2 = peer1.peers["peer2"];
+    expect(peer1ToPeer2.reliableConnection).toBeDefined();
+    expect(peer1ToPeer2.reliableConnection.writable).toBeTrue();
   });
 });
