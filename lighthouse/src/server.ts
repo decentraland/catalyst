36c03c31e02d97d43445bbae88ff2a74a7defb49
pablitar <pablitar@gmail.com>
2019-12-16 17:18:28 -0300

    Extracted rooms service

--- lighthouse/src/server.ts
+++ lighthouse/src/server.ts
@@ -1,16 +1,15 @@
 import cors from "cors";
 import express from "express";
+import { Response } from "express";
 import morgan from "morgan";
 import { ExpressPeerServer } from "peerjs-server";
 import { Peer, RelayMode } from "../../peer/src/Peer";
-import { PeerConnectionData } from "../../peer/src/types";
 import * as wrtc from "wrtc";
 import WebSocket from "ws";
+import { RoomsService } from "./roomsService";
 
 const relay = true;
 const port = process.env.PORT ?? 9000;
-
-const rooms: Record<string, PeerConnectionData[]> = {};
 
 let peer: Peer;
 
@@ -19,6 +18,32 @@ let peer: Peer;
 // });
 
 const app = express();
+
+//Validations
+function requireParameters(
+  paramNames: string[],
+  obj: object,
+  response: Response,
+  then: () => void
+) {
+  const missing = paramNames.filter(param => typeof obj[param] === "undefined");
+
+  if (missing.length > 0) {
+    response.status(400).send({
+      status: "bad-request",
+      message: `Missing required parameters: ${missing.join(",")}`
+    });
+  } else {
+    then();
+  }
+}
+
+//Services
+const roomsService = new RoomsService({
+  relay,
+  serverPeerProvider: () => peer
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("combined"));
@@ -27,43 +52,38 @@ app.get("/hello", (req, res, next) => {
   res.send("Hello world!!!");
 });
 
-// GET /rooms[?userId=] -> returns list of rooms. Includes users per room by default. If a userId is specified, it returns the rooms which that user has joined.
+// GET /rooms[?userId=] -> returns list of rooms. If a userId is specified, it returns the rooms which that user has joined.
 app.get("/rooms", (req, res, next) => {
-  const { userId } = req.query;
-  const _rooms = userId
-    ? Object.entries(rooms)
-        .filter(([, users]) => users.some(user => user.userId === userId))
-        .map(([id]) => id)
-    : Object.keys(rooms);
-  res.send(_rooms);
+  res.send(roomsService.getRoomIds({ userId: req.query.userId }));
 });
 
 // GET /room/:id -> returns list of users in a room with :id
 app.get("/rooms/:roomId", (req, res, next) => {
-  res.send(rooms[req.params.roomId]);
+  const roomUsers = roomsService.getUsers(req.params.roomId);
+  if (typeof roomUsers === "undefined") {
+    res.status(404).send({ status: "not-found" });
+  } else {
+    res.send(roomUsers);
+  }
 });
 
 // PUT /room/:id { userid, nickname } -> adds a user to a particular room. If the room doesn’t exists, it creates it.
 app.put("/rooms/:roomId", async (req, res, next) => {
   const { roomId } = req.params;
-  let room = rooms[roomId];
-  if (!room) {
-    rooms[roomId] = room = [];
-    // if relaying peer exists, add to room when it's created
-    if (relay) {
-      await peer?.joinRoom(roomId);
+  requireParameters(["userId", "peerId"], req.body, res, async () => {
+    try {
+      const room = await roomsService.addUserToRoom(roomId, req.body);
+      res.send(room);
+    } catch (err) {
+      next(err);
     }
-  }
-  if (!room.some($ => $.userId === req.body.id)) {
-    room.push(relay ? { ...req.body, peerId: peer.nickname } : req.body);
-  }
-  res.send(room);
+  });
 });
 
 // DELETE /room/:id/:userId -> deletes a user from a room. If the room remains empty, it deletes the room.
 app.delete("/rooms/:roomId/users/:userId", (req, res, next) => {
   const { roomId, userId } = req.params;
-  removeUserFromRoom(roomId, userId);
+  roomsService.removeUserFromRoom(roomId, userId);
   res.end();
 });
 
@@ -88,26 +108,6 @@ const server = app.listen(port, () => {
   }
 });
 
-function removeUserFromRoom(roomId: string, userId: string) {
-  let room = rooms[roomId];
-  if (room) {
-    const index = room.indexOf(room.find($ => $.userId === userId) as any);
-    if (index !== -1) {
-      room.splice(index, 1);
-    }
-  }
-  if (room.length === 0) {
-    delete rooms[roomId];
-  }
-}
-
-function removeUser(userId: string) {
-  Object.keys(rooms).forEach(room => removeUserFromRoom(room, userId));
-  if(peer) {
-     peer.disconnectFrom(userId)
-  }
-}
-
 const options = {
   debug: true,
   path: "/"
@@ -116,8 +116,11 @@ const options = {
 const peerServer = ExpressPeerServer(server, options);
 
 peerServer.on("disconnect", client => {
-  console.log("User disconnected from server socket. Removing from all rooms: " + client.id)
-  removeUser(client.id);
+  console.log(
+    "User disconnected from server socket. Removing from all rooms: " +
+      client.id
+  );
+  roomsService.removeUser(client.id);
 });
 
 peerServer.on("error", console.log);
