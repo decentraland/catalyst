8a5b01d0ce77492dcd02682e901a2de766f1cd71
Pablo de Haro <pablitar@gmail.com>
2019-12-24 11:53:00 -0300

    Stress test devserver working

--- peer/scripts/stress-test/index.ts
+++ peer/scripts/stress-test/index.ts
@@ -1,18 +1,25 @@
 import { Peer } from "../../src/Peer";
+import { util } from "../../src/peerjs-server-connector/util";
 
 function sleep(time: number) {
   return new Promise<null>(resolve => {
-    setTimeout(resolve, time)
-  })
+    setTimeout(resolve, time);
+  });
 }
+
+const sessionId = util.randomToken();
 
 (async () => {
   const numberOfPeers = 5;
-  const messageCount = 500;
-  const timeBetweenMessages = 50; 
+  const messageCount = 200;
+  const timeBetweenMessages = 50;
+
   const globalStats = {
     messagesSent: 0,
-    messagesReceived: 0
+    messagesReceived: 0,
+    totalLatency: 0,
+    averageLatency: 0,
+    testStarted: new Date().getTime()
   };
 
   console.log("Creating " + numberOfPeers + " peers");
@@ -20,28 +27,38 @@ function sleep(time: number) {
   type PeerContainer = {
     messagesSent: number;
     messagesReceived: number;
-    countMessage(msgDetails: any): void;
-    peer: Peer;
+    countReceived(): void;
+    countSent(): void;
+    peer?: Peer;
   };
 
   const peers: PeerContainer[] = [];
+  const finishedPeers: PeerContainer[] = [];
 
   for (let i = 0; i < numberOfPeers; i++) {
     const peerContainer: PeerContainer = {
       messagesSent: 0,
       messagesReceived: 0,
-      countMessage(msgDetails) {
+      countReceived() {
         this.messagesReceived += 1;
         globalStats.messagesReceived += 1;
       },
-      peer: undefined as Peer
+      countSent() {
+        this.messagesSent += 1;
+        globalStats.messagesSent += 1;
+      }
     };
 
     const peer = new Peer(
-      "http://localhost:9000/",
-      "peer" + i,
+      "http://localhost:9000",
+      `peer_${sessionId}_${i}`,
       (sender, room, payload) => {
-        peerContainer.countMessage({ sender, room, payload });
+        peerContainer.countReceived();
+        const { stamp } = payload;
+        const latency = new Date().getTime() - stamp;
+        globalStats.totalLatency += latency;
+        globalStats.averageLatency =
+          globalStats.totalLatency / globalStats.messagesReceived;
       }
     );
 
@@ -50,14 +67,32 @@ function sleep(time: number) {
     peers.push(peerContainer);
   }
 
-  await Promise.all(peers.map(pc => pc.peer.joinRoom("room")));
+  await Promise.all(peers.map(pc => pc.peer!.joinRoom("room")));
 
-  function startSendingMessages(peerContainer: PeerContainer) {
-    await()
-    const doSendMessage = () => {
+  function doSendMessage(container: PeerContainer) {
+    const messageId = util.randomToken();
+    container.peer!.sendMessage("room", {
+      test: "this is a test",
+      messageId,
+      stamp: new Date().getTime()
+    });
+    container.countSent();
+  }
 
-      peerContainer.peer.sendMessage("room", { test: "this is a test" });
+  async function startSendingMessages(peerContainer: PeerContainer) {
+    const send = () => {
+      doSendMessage(peerContainer);
+      if (peerContainer.messagesSent < messageCount) {
+        setTimeout(send, timeBetweenMessages);
+      } else {
+        console.log("Peer finished: " + peerContainer.peer!.nickname);
+        finishedPeers.push(peerContainer);
+      }
     };
+
+    await sleep(Math.floor(Math.random() * timeBetweenMessages));
+
+    send();
   }
 
   setTimeout(() => {
@@ -65,4 +100,30 @@ function sleep(time: number) {
       startSendingMessages(it);
     });
   }, 2000);
+
+  function setText(id: string, text: any) {
+    document.getElementById(id)!.innerText = text.toString();
+  }
+
+  function updateStats() {
+    const currentTime = new Date().getTime();
+    const remainingPeers = peers.length - finishedPeers.length;
+    setText("peers", peers.length);
+    setText("peerssending", remainingPeers);
+    setText("sent", globalStats.messagesSent);
+    setText("received", globalStats.messagesReceived);
+    setText("latency", globalStats.averageLatency);
+    if (remainingPeers > 0) {
+      const runtime = currentTime - globalStats.testStarted;
+      setText("sentpersecond", (globalStats.messagesSent * 1000) / runtime);
+      setText(
+        "receivedpersecond",
+        (globalStats.messagesReceived * 1000) / runtime
+      );
+    }
+
+    setTimeout(updateStats, 500);
+  }
+
+  updateStats();
 })();
