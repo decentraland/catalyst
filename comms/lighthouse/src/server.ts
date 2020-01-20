3bacfc74425f0d42dd9b80d1d0730f75f4524fa8
moliva <oliva.miguelh@gmail.com>
2020-01-20 20:02:40 -0300

    improving monitoring of lighthouse layers loading

--- comms/lighthouse/src/server.ts
+++ comms/lighthouse/src/server.ts
@@ -7,7 +7,7 @@ import { recover } from "web3x/utils";
 import { PeersService } from "./peersService";
 import { configureRoutes } from "./routes";
 import { LayersService } from "./layersService";
-import { Metrics } from '../../../commons/src/metrics';
+import { Metrics } from "../../../commons/src/metrics";
 import { IMessage } from "peerjs-server/dist/src/models/message";
 import { MessageType } from "peerjs-server/dist/src/enums";
 import * as path from "path";
@@ -95,9 +95,6 @@ app.use("/peerjs", peerServer);
 
 const monitorServer = MonitorServer();
 
-const patho = path.join(__dirname, "../../../../npm/node_modules")
-console.log(`patho ${patho}`)
-app.use("/static", express.static(patho));
 app.use("/monitor", monitorServer);
 
 function MonitorServer() {
@@ -110,32 +107,79 @@ function MonitorServer() {
         <title>Decentraland Lighthouse</title>
       </head>
      
-      <body><div id="root">
-      <script src="https://github.com/mdaines/viz.js/releases/download/v2.1.2/viz.js"></script>
-      <script src="https://github.com/mdaines/viz.js/releases/download/v2.1.2/full.render.js"></script>
-      <script>
-        async function renderTopology() {
-          const viz = new Viz();
+      <body>
+        <div id="root">
+          <script>
+            async function renderTopology() {
 
-          const response = await fetch("/layers/blue/topology?format=graphviz")
-          const topology = await response.text()
-          console.log('topology', topology)
-          
-          viz.renderSVGElement(topology)
-          .then(function(element) {
-            document.body.appendChild(element);
-          })
-          .catch(error => {
-            // Create a new Viz instance (@see Caveats page for more info)
-            viz = new Viz();
-        
-            // Possibly display the error
-            console.error(error);
-          });
-        }
-        renderTopology()
-      </script>
-      </div></body>
+              const layersResponse = await fetch("/layers")
+              const layers = await layersResponse.json()
+
+              if (layers.length === 0) {
+                const element = document.createElement("p");
+                const text = document.createTextNode("No layers found! Join peers first maybe?");
+                element.appendChild(text);
+                document.body.appendChild(element);
+              } else {
+                renderLayers(layers)
+              }
+
+            }
+
+            async function event(precondition) {
+              const checkVizModuleLoaded = (resolve, reject, attempt) => {
+                if (precondition()) {
+                  resolve();
+                } else if (attempt > 1000) {
+                  reject(new Error('viz module not loaded'));
+                } else {
+                  setTimeout(() => checkVizModuleLoaded(resolve, reject, attempt + 1), 100);
+                }
+              }
+
+              return new Promise((resolve, reject) => {
+                checkVizModuleLoaded(resolve, reject, 0)
+              });
+            }
+
+            async function renderLayers(layers) {
+              await event(() => typeof Viz !== "undefined");
+              await event(() => typeof Viz.render !== "undefined");
+
+              const viz = new Viz();
+
+              for (const layer of layers) {
+                const h1 = document.createElement("h1");
+                const text = document.createTextNode("Layer " + layer);
+                h1.appendChild(text);
+                document.body.appendChild(h1);
+
+                const response = await fetch("/layers/blue/topology?format=graphviz")
+                const topology = await response.text()
+                console.log('topology', topology)
+
+                viz.renderSVGElement(topology)
+                  .then(element => document.body.appendChild(element))
+                  .catch(error => {
+                    // Create a new Viz instance (@see Caveats page for more info)
+                    viz = new Viz();
+
+                    const element = document.createElement("p");
+                    const text = document.createTextNode("Error while rendering layer");
+                    element.appendChild(text);
+                    document.body.appendChild(element);
+
+                    console.error(error);
+                  });
+              }
+            }
+
+            renderTopology()
+          </script>
+          <script src="https://github.com/mdaines/viz.js/releases/download/v2.1.2/viz.js"></script>
+          <script src="https://github.com/mdaines/viz.js/releases/download/v2.1.2/full.render.js"></script>
+        </div>
+      </body>
       </html>`
     );
   };
