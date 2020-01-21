b814ca596de8373a20f3dc166ad9b388c3e0e0a2
moliva <oliva.miguelh@gmail.com>
2020-01-20 21:02:25 -0300

    adding static routes for monitor

--- comms/lighthouse/src/server.ts
+++ comms/lighthouse/src/server.ts
@@ -8,6 +8,7 @@ import { LayersService } from "./layersService";
 import { Metrics } from "../../../commons/src/metrics";
 import { IMessage } from "peerjs-server/dist/src/models/message";
 import { MessageType } from "peerjs-server/dist/src/enums";
+import * as path from "path";
 
 const relay = parseBoolean(process.env.RELAY ?? "false");
 const accessLogs = parseBoolean(process.env.ACCESS ?? "false");
@@ -77,94 +78,6 @@ export function getPeerJsRealm(): IRealm {
 
 app.use("/peerjs", peerServer);
 
-const monitorServer = MonitorServer();
+const _static = path.join(__dirname, "../static");
 
-app.use("/monitor", monitorServer);
-
-function MonitorServer() {
-  return (req, res, next) => {
-    res.send(
-      `
-      <!DOCTYPE html>
-      <html lang="en"><head>
-      <meta charset="UTF-8">
-        <title>Decentraland Lighthouse</title>
-      </head>
-     
-      <body>
-        <div id="root">
-          <script>
-            async function renderTopology() {
-
-              const layersResponse = await fetch("/layers")
-              const layers = await layersResponse.json()
-
-              if (layers.length === 0) {
-                const element = document.createElement("p");
-                const text = document.createTextNode("No layers found! Join peers first maybe?");
-                element.appendChild(text);
-                document.body.appendChild(element);
-              } else {
-                renderLayers(layers)
-              }
-
-            }
-
-            async function event(precondition) {
-              const checkVizModuleLoaded = (resolve, reject, attempt) => {
-                if (precondition()) {
-                  resolve();
-                } else if (attempt > 1000) {
-                  reject(new Error('viz module not loaded'));
-                } else {
-                  setTimeout(() => checkVizModuleLoaded(resolve, reject, attempt + 1), 100);
-                }
-              }
-
-              return new Promise((resolve, reject) => {
-                checkVizModuleLoaded(resolve, reject, 0)
-              });
-            }
-
-            async function renderLayers(layers) {
-              await event(() => typeof Viz !== "undefined");
-              await event(() => typeof Viz.render !== "undefined");
-
-              const viz = new Viz();
-
-              for (const layer of layers) {
-                const h1 = document.createElement("h1");
-                const text = document.createTextNode("Layer " + layer);
-                h1.appendChild(text);
-                document.body.appendChild(h1);
-
-                const response = await fetch("/layers/blue/topology?format=graphviz")
-                const topology = await response.text()
-                console.log('topology', topology)
-
-                viz.renderSVGElement(topology)
-                  .then(element => document.body.appendChild(element))
-                  .catch(error => {
-                    // Create a new Viz instance (@see Caveats page for more info)
-                    viz = new Viz();
-
-                    const element = document.createElement("p");
-                    const text = document.createTextNode("Error while rendering layer");
-                    element.appendChild(text);
-                    document.body.appendChild(element);
-
-                    console.error(error);
-                  });
-              }
-            }
-
-            renderTopology()
-          </script>
-          <script src="https://github.com/mdaines/viz.js/releases/download/v2.1.2/viz.js"></script>
-          <script src="https://github.com/mdaines/viz.js/releases/download/v2.1.2/full.render.js"></script>
-        </div>
-      </body>
-      </html>`
-    );
-  };
-}
+app.use("/monitor", express.static(_static + "/monitor"));
