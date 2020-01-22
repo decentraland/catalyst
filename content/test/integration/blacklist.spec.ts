aa8b5fc0b7abcb3f7fc0bfe04828acc6e4d1a30a
Miguel Gagliardo <miguel@decentraland.org>
2020-01-22 14:11:07 -0300

    Fix: Changing metrics port to 9090 (#23)

--- content/test/integration/blacklist.spec.ts
+++ content/test/integration/blacklist.spec.ts
@@ -1,4 +1,4 @@
-import { EnvironmentBuilder } from "@katalyst/content/Environment"
+import { EnvironmentConfig, EnvironmentBuilder } from "@katalyst/content/Environment"
 import { EntityType } from "@katalyst/content/service/Entity"
 import { BlacklistServiceDecorator } from "@katalyst/content/blacklist/BlacklistServiceDecorator"
 import { buildDeployData, deleteServerStorage } from "./E2ETestUtils"
@@ -19,6 +19,7 @@ describe("End 2 end - Blacklist", () => {
             .withAnalytics(new MockedContentAnalytics())
             .withSynchronizationManager(new MockedSynchronizationManager())
             .withAccessChecker(new MockedAccessChecker())
+            .withConfig(EnvironmentConfig.METRICS, false)
             .build()
         server = new TestServer(env)
         await server.start()
