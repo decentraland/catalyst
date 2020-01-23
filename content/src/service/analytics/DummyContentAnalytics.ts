b07e0fb5b5f0da3c125ec6c8c56d8747daa46ab2
Marcos NC <marcosnc@gmail.com>
2020-01-23 16:38:10 -0300

    Reduced deployment log.

--- content/src/service/analytics/DummyContentAnalytics.ts
+++ content/src/service/analytics/DummyContentAnalytics.ts
@@ -1,12 +1,14 @@
 import { Entity } from "../Entity";
 import { ContentAnalytics } from "./ContentAnalytics";
-import { ContentAnalyticsWithSegment } from "./ContentAnalyticsWithSegment";
 import { EthAddress } from "../auth/Authenticator";
 
 export class DummyContentAnalytics implements ContentAnalytics {
 
     recordDeployment(serverName: string, entity: Entity, ethAddress: EthAddress, origin: string): void {
-        console.log("Analytics Deployment Record: ", JSON.stringify(ContentAnalyticsWithSegment.createRecordEvent(serverName, entity, ethAddress, origin)))
+        console.info(this.createLogLine(serverName, entity, ethAddress, origin))
 	}
 
+    private createLogLine(serverName: string, entity: Entity, ethAddress: EthAddress, origin: string) {
+        return `Deployment. Server: ${serverName}. Entity: ${entity.id}.`
+    }
 }
