ee020c06f9e38c1aed79df2130076f36948108d6
Marcos NC <marcosnc@gmail.com>
2020-01-24 16:57:42 -0300

    Using thegraph to filter ENS.

--- lambdas/src/apis/profiles/ensFiltering.ts
+++ lambdas/src/apis/profiles/ensFiltering.ts
@@ -1,7 +1,38 @@
-export function ownsENS(ethAddress: string, ens: string): Promise<boolean> {
-    return Promise.resolve(ENS_TABLE.filter(item => item.ens===ens && item.ethAddress===ethAddress).length > 0)
+import fetch from "node-fetch"
+
+const query = `
+  query GetNameByBeneficiary($beneficiary: String) {
+    nameRegistrations(where:{beneficiary:$beneficiary}) {
+      labelHash
+      beneficiary
+      caller
+      subdomain
+      createdAt
+    }
+  }`
+const url = "https://api.thegraph.com/subgraphs/name/nicosantangelo/testing";
+const opts = (ethAddress) => ({
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query, variables: { beneficiary: ethAddress } })
+})
+
+export async function getOwnedENS(ethAddress: string): Promise<string[]> {
+    try {
+        const response = await fetch(url, opts(ethAddress))
+        if (response.ok) {
+            const jsonResponse: GraphResponse = await response.json()
+            return jsonResponse.data.nameRegistrations.map(registration => registration.subdomain)
+        }
+    } catch (error) {
+        console.log(`Could not retrieve ENS for address ${ethAddress}.`, error)
+    }
+    return []
 }
 
-const ENS_TABLE: {ens:string, ethAddress:string}[] = [
-    {ens: "Skeenee", ethAddress: "0x187dad1e0dd5e599091c2ec88e89afb4cde4b7a3"},
-]
+type GraphResponse = {
+    data: {
+        nameRegistrations: {subdomain: string}[]
+    }
+}
+
