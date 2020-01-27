728a9f50115ed9d99777c12f39de8e1cdb46ed98
nchamo <nicolas@chamo.com.ar>
2020-01-27 11:58:15 -0300

    Fix tests

--- content/test/unit/blacklist/BlacklistServiceDecorator.spec.ts
+++ content/test/unit/blacklist/BlacklistServiceDecorator.spec.ts
@@ -12,8 +12,8 @@ import { Authenticator } from "@katalyst/content/service/auth/Authenticator";
 
 describe("BlacklistServiceDecorator", () => {
 
-    const P1: Pointer = "P1"
-    const P2: Pointer = "P2"
+    const P1: Pointer = "p1"
+    const P2: Pointer = "p2"
     const content1 = buildRandomContent()
     const content2 = buildRandomContent()
     const ethAddress = random.alphaNumeric(10)
