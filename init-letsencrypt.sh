d30db41a32c65e9c41aa34ccbd5b7f42cbdac4f8
Alejandro Drabenche <alejandro@decentraland.org>
2020-01-30 13:43:25 -0300

    Testing nginx

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 PROGNAME=$0
-staging=1
 
 ####
 # Functions
@@ -25,8 +24,10 @@ leCertEmit () {
   rsa_key_size=4096
   data_path="./local/certbot"
   email="alejandro@decentraland.zone" # Adding a valid address is strongly recommended
-  staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
-
+  if test -z staging; then
+    echo "## requeting a staging certificate"
+    staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+  fi
   if [ -d "$data_path" ]; then
     read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
     if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
