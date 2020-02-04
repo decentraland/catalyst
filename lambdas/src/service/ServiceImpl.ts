c9a53aea93aec29bf2a88c88c7f4b6d761b38970
Marcos NC <marcosnc@gmail.com>
2020-02-04 15:37:17 -0300

    Added configuration and commit information to lambdas status page.

--- lambdas/src/service/ServiceImpl.ts
+++ lambdas/src/service/ServiceImpl.ts
@@ -1,11 +1,16 @@
 import { LambdasService, ServerStatus } from "./Service";
+import { Environment, EnvironmentConfig } from "../Environment";
 
 export class ServiceImpl implements LambdasService {
+
+    constructor(private readonly env: Environment) { }
 
     getStatus(): Promise<ServerStatus> {
         return Promise.resolve({
             version: "1.0",
             currentTime: Date.now(),
+            contentServerUrl: this.env.getConfig(EnvironmentConfig.CONTENT_SERVER_ADDRESS),
+            commitHash: this.env.getConfig(EnvironmentConfig.COMMIT_HASH),
         })
     }
 
