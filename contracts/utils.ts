f4fc36cc366c73bd9e7ed268e981fda958e93be7
moliva <oliva.miguelh@gmail.com>
2020-02-11 16:26:52 -0300

    using new project id

--- contracts/utils.ts
+++ contracts/utils.ts
@@ -5,8 +5,8 @@ import { Catalyst } from "./Catalyst";
 
 export const networks = {
   ropsten: {
-    wss: "wss://ropsten.infura.io/ws/v3/d5eed066e203446197a3c9fa03a8a1ba",
-    http: "https://ropsten.infura.io/v3/d5eed066e203446197a3c9fa03a8a1ba",
+    wss: "wss://ropsten.infura.io/ws/v3/2c902c2e3b8947d3b34bba7ca48635fc",
+    http: "https://ropsten.infura.io/v3/2c902c2e3b8947d3b34bba7ca48635fc",
     contracts: {
       catalyst: {
         address: "0xadd085f2318e9678bbb18b3e0711328f902b374b",
@@ -15,8 +15,8 @@ export const networks = {
     }
   },
   mainnet: {
-    wss: "wss://mainnet.infura.io/ws/v3/d5eed066e203446197a3c9fa03a8a1ba",
-    http: "https://mainnet.infura.io/v3/d5eed066e203446197a3c9fa03a8a1ba",
+    wss: "wss://mainnet.infura.io/ws/v3/2c902c2e3b8947d3b34bba7ca48635fc",
+    http: "https://mainnet.infura.io/v3/2c902c2e3b8947d3b34bba7ca48635fc",
     contracts: {
       catalyst: {
         address: "0x4a2f10076101650f40342885b99b6b101d83c486",
