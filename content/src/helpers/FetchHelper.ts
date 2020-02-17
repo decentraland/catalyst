83520eeafdb90d368d3035eb99e7e2de55a0aebd
nchamo <nicolas@chamo.com.ar>
2020-02-17 12:36:19 -0300

    Add await

--- content/src/helpers/FetchHelper.ts
+++ content/src/helpers/FetchHelper.ts
@@ -12,24 +12,24 @@ export class FetchHelper {
         private readonly fileDownloadRequestTimeout: number = ms('1m')) { }
 
     async fetchJson(url: string): Promise<any> {
-        return  FetchHelper.fetchInternal(url, response => response.json(), this.jsonRequestTimeout)
+        return FetchHelper.fetchInternal(url, response => response.json(), this.jsonRequestTimeout)
     }
 
     async fetchBuffer(url: string): Promise<Buffer> {
-        return  FetchHelper.fetchInternal(url, response => response.buffer(), this.fileDownloadRequestTimeout)
+        return FetchHelper.fetchInternal(url, response => response.buffer(), this.fileDownloadRequestTimeout)
     }
 
     private static async fetchInternal<T>(url: string, responseConsumer: (response) => Promise<T>, maxWaitingTime: number): Promise<T> {
         const controller = new AbortController();
         const timeout = setTimeout(() => {
-            controller.abort();
             FetchHelper.LOGGER.warn(`Request to url ${url} exceeded the max waiting time. It took more than ${maxWaitingTime} millis.`);
+            controller.abort();
         }, maxWaitingTime);
 
         try {
             const response = await fetch(url, { signal: controller.signal });
             if (response.ok) {
-                return responseConsumer(response)
+                return await responseConsumer(response)
             } else {
                 throw new Error(`Failed to fetch ${url}. Got status ${response.status}, ${response.statusText}`)
             }
