7304428a4dcffdd5136b417b7dd5c63af021ec44
pablitar <pablo@decentraland.org>
2020-02-27 11:33:22 -0300

    fix: Making simple storage concurrency safe (#78)

--- comms/lighthouse/test/naming.spec.ts
+++ comms/lighthouse/test/naming.spec.ts
@@ -17,7 +17,7 @@ const daoClient: DAOClient = {
   }
 };
 
-const existingName = "fenrir";
+let existingName = "fenrir";
 
 describe("picking a name", function() {
   beforeAll(() => {
@@ -40,18 +40,18 @@ describe("picking a name", function() {
 
       expect(name).not.toBe(existingName);
       expect(defaultNames.includes(name)).toBe(true);
-      
+
       await lighthouseStorage.clear();
     }
   });
 
-  it("picks up a configured name when the name is no in the DAO", async () => {
+  it("picks up a configured name when the name is not in the DAO", async () => {
     for (let i = 0; i < 20; i++) {
       const name = await pickName("rick,morty", daoClient);
 
       expect(name).not.toBe(existingName);
       expect(["rick", "morty"].includes(name)).toBe(true);
-      
+
       await lighthouseStorage.clear();
     }
   });
@@ -62,5 +62,24 @@ describe("picking a name", function() {
     const currentName = await pickName(undefined, daoClient);
 
     expect(currentName).toBe(previousName);
+  });
+
+  it("doesn't reuse the previous name if it was taken", async () => {
+    const previousName = await pickName(undefined, daoClient);
+
+    existingName = previousName;
+
+    const currentName = await pickName(undefined, daoClient);
+
+    expect(currentName).not.toBe(previousName);
+  });
+
+  it("fails if it can't use any of the names available", async () => {
+    try {
+      await pickName(existingName, daoClient);
+      fail();
+    } catch (e) {
+      expect(e.message).toBe("Could not set my name! Names taken: " + existingName);
+    }
   });
 });
