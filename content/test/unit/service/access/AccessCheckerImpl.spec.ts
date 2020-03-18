541216b9542e22a61d75539a1a87a693f4c2b6c9
Nicolas Chamo <nicolas@chamo.com.ar>
2020-03-18 14:40:22 -0300

    fix: scene validation can be done at any moment
    * Checking current time and also 5 min in the past
    
    * Ignore test
    
    * Move integration test out of unit test
    
    * Test rename

--- content/test/unit/service/access/AccessCheckerImpl.spec.ts
+++ content/test/unit/service/access/AccessCheckerImpl.spec.ts
@@ -37,15 +37,6 @@ describe("AccessCheckerImpl", function () {
         expect(errors.length).toBe(0)
     })
 
-    it(`When access URL is wrong it reports an error`, async () => {
-        const accessChecker = new AccessCheckerImpl(new ContentAuthenticator(), "Wrong URL");
-
-        const errors = await accessChecker.hasAccess(EntityType.SCENE, ["102,4"], Date.now(), "Some-address-without-permissions");
-
-        expect(errors.length).toBe(1)
-        expect(errors[0]).toEqual("The provided Eth Address does not have access to the following parcel: (102,4)")
-    })
-
     it(`When an address without permissions tries to deploy it fails`, async () => {
         const accessChecker = new AccessCheckerImpl(new ContentAuthenticator(), DEFAULT_DCL_PARCEL_ACCESS_URL);
 
