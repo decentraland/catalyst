c644d696015b87b4d8d6901ed6a9900f4df5cec9
Pablo de Haro <pablitar@gmail.com>
2020-03-19 15:13:59 -0300

    Changed discretize function to make far away peers equivalent

--- commons/utils/Positions.ts
+++ commons/utils/Positions.ts
@@ -1,9 +1,10 @@
-export const DISCRETIZE_POSITION_INTERVAL = 50;
+export const DISCRETIZE_POSITION_INTERVALS = [32, 64, 80];
 
 export type Position3D = [number, number, number];
 export type Position2D = [number, number];
 
 export type Position = Position2D | Position3D;
+
 
 export function isPosition3D(position: any): position is Position3D {
   return position instanceof Array && position.length === 3;
@@ -13,7 +14,24 @@ export function isPosition2D(position: any): position is Position2D {
   return position instanceof Array && position.length === 2;
 }
 
-export function discretizedPositionDistance(a: Position, b: Position, interval: number = DISCRETIZE_POSITION_INTERVAL) {
+/**
+ * Calculates the discretized distance between position a and position b, using the provided intervals (DISCRETIZE_POSITION_INTERVALS as default)
+ * 
+ * For instance, given the intervals [32, 64, 80], then we get the following values:
+ * - distance(a, b) = 30 => 0
+ * - distance(a, b) = 50 => 1
+ * - distance(a, b) = 64 => 1
+ * - distance(a, b) = 77 => 2
+ * - distance(a, b) = 90 => 3
+ * - distance(a, b) = 99999 => 3
+ * 
+ * The @param intervals provided should be ordered from lower to greater
+ * 
+ * @param a 
+ * @param b 
+ * @param intervals 
+ */
+export function discretizedPositionDistance(a: Position, b: Position, intervals: number[] = DISCRETIZE_POSITION_INTERVALS) {
   let dx = 0;
   let dy = 0;
   let dz = 0;
@@ -25,9 +43,11 @@ export function discretizedPositionDistance(a: Position, b: Position, interval: 
     dz = a[2] - b[2];
   }
 
-  const distance = Math.sqrt((dx * dx + dy * dy + dz * dz))
+  const squaredDistance = (dx * dx + dy * dy + dz * dz)
 
-  return Math.floor(distance / interval);
+  const intervalIndex = intervals.findIndex(it => squaredDistance <= it * it)
+
+  return intervalIndex !== -1 ? intervalIndex : intervals.length;
 }
 
 export type PeerConnectionHint = {
--- commons/utils/test/Positions.spec.ts
+++ commons/utils/test/Positions.spec.ts
@@ -2,14 +2,14 @@ import { Position3D, discretizedPositionDistance } from "decentraland-katalyst-u
 
 describe("Discretize Positions", () => {
   it("should convert close positions to equivalent", () => {
-    const position1: Position3D = [80, 80, 80];
-    const position2: Position3D = [85, 85, 85];
+    const position1: Position3D = [15, 15, 15];
+    const position2: Position3D = [10, 10, 10];
     const origin: Position3D = [0, 0, 0]
 
     expect(discretizedPositionDistance(origin, position1)).toEqual(discretizedPositionDistance(origin, position2))    
   })
 
-  it("should preserve higher distances when it is greater than the discretize interval", () => {
+  it("should preserve higher distances when they are in different intervals", () => {
     const position1: Position3D = [20, 20, 20];
     const position2: Position3D = [90, 90, 90];
     const origin: Position3D = [0, 0, 0]
@@ -17,13 +17,23 @@ describe("Discretize Positions", () => {
     expect(discretizedPositionDistance(origin, position1)).toBeLessThan(discretizedPositionDistance(origin, position2))    
   })
 
-  it("should calculate according to the interval", () => {
+  it("should preserve make distances higher than the last interval equivalent", () => {
+    const position1: Position3D = [990, 990, 990];
+    const position2: Position3D = [44400, 44004, 44444];
     const origin: Position3D = [0, 0, 0]
 
-    expect(discretizedPositionDistance(origin, [50, 0, 0], 50)).toEqual(1)
-    expect(discretizedPositionDistance(origin, [0, 100, 0], 50)).toEqual(2)
-    expect(discretizedPositionDistance(origin, [0, 0, 150], 50)).toEqual(3)
-    expect(discretizedPositionDistance(origin, [100, 100, 100], 50)).toEqual(3)
-    expect(discretizedPositionDistance(origin, [150, 150, 150], 50)).toEqual(5)        
+    expect(discretizedPositionDistance(origin, position1)).toEqual(discretizedPositionDistance(origin, position2))    
+  })
+
+  it("should calculate according to the intervals", () => {
+    const origin: Position3D = [0, 0, 0]
+
+    expect(discretizedPositionDistance(origin, [20, 0, 0])).toEqual(0)
+    expect(discretizedPositionDistance(origin, [0, 32, 0])).toEqual(0)
+    expect(discretizedPositionDistance(origin, [0, 0, 48])).toEqual(1)
+    expect(discretizedPositionDistance(origin, [30, 30, 30])).toEqual(1)
+    expect(discretizedPositionDistance(origin, [72, 0, 0])).toEqual(2)
+    expect(discretizedPositionDistance(origin, [60, 60, 60])).toEqual(3)
+    expect(discretizedPositionDistance(origin, [150, 150, 150])).toEqual(3)        
   })
 })
