4396036f958557247f1d84da4955263d05163e5d
moliva <oliva.miguelh@gmail.com>
2020-03-27 14:52:17 -0300

    wip

--- lambdas/src/controller/ControllerFactory.ts
+++
@@ -1,8 +0,0 @@
-import { Environment, Bean } from "../Environment";
-import { Controller } from "./Controller";
-
-export class ControllerFactory {
-    static create(env: Environment): Controller {
-        return new Controller(env.getBean(Bean.SERVICE));
-    }
-}
