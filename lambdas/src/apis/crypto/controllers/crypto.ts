e5e4ddb4056588ad29d14bea9a3075748285be7a
Marcos NC <marcosnc@gmail.com>
2020-04-07 18:24:53 -0300

    Some fixes.

--- lambdas/src/apis/crypto/controllers/crypto.ts
+++ lambdas/src/apis/crypto/controllers/crypto.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from 'express'
 import { Environment, EnvironmentConfig } from '../../../Environment'
-import { Timestamp, AuthLink, Authenticator, ValidationResult } from 'dcl-crypto';
+import { AuthLink, Authenticator, ValidationResult } from 'dcl-crypto';
 import { httpProviderForNetwork } from 'decentraland-katalyst-contracts/utils';
 
 export async function validateSignature(env: Environment, req: Request, res: Response) {
     // Method: POST
     // Path: /validate-signature
-    const timestamp: Timestamp  = req.body.timestamp;
+    const timestamp: string  = req.body.timestamp;
     const authChain: AuthLink[] = req.body.authChain;
 
-    const result: ValidationResult = await Authenticator.validateSignature(timestamp.toString(), authChain, httpProviderForNetwork(env.getConfig(EnvironmentConfig.ETH_NETWORK)))
+    const result: ValidationResult = await Authenticator.validateSignature(timestamp, authChain, httpProviderForNetwork(env.getConfig(EnvironmentConfig.ETH_NETWORK)))
 
     res.send({
         valid: result.ok,
