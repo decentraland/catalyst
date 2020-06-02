3ab40a55648e44a02298a20596dbb8f23435a1f9
Nicolas Chamo <nicolas@chamo.com.ar>
2020-06-02 13:57:47 -0300

    fix: handle entities with same timestamp

--- content/src/storage/repositories/PointerHistoryRepository.ts
+++ content/src/storage/repositories/PointerHistoryRepository.ts
@@ -15,10 +15,10 @@ export class PointerHistoryRepository {
                 LEFT JOIN deployments AS dep2 ON dep1.deleter_deployment = dep2.id
                 WHERE pointer_history.entity_type = $1 AND
                     pointer_history.pointer IN ($2:list) AND
-                    dep1.entity_timestamp <= to_timestamp($3 / 1000.0) AND
-                    (dep2.id IS NULL OR dep2.entity_timestamp > to_timestamp($3 / 1000.0))
+                    (dep1.entity_timestamp < to_timestamp($3 / 1000.0) OR (dep1.entity_timestamp = to_timestamp($3 / 1000.0) AND dep1.entity_id < $4)) AND
+                    (dep2.id IS NULL OR dep2.entity_timestamp > to_timestamp($3 / 1000.0) OR (dep2.entity_timestamp = to_timestamp($3 / 1000.0) AND dep2.entity_id > $4))
                 ORDER BY pointer_history.pointer, dep1.entity_timestamp DESC, dep1.entity_id DESC`,
-                [entity.type, entity.pointers, entity.timestamp],
+                [entity.type, entity.pointers, entity.timestamp, entity.id],
                 row => row.id)
 
             const overwrittenBy: DeploymentId | null = await task.oneOrNone(`
@@ -27,10 +27,10 @@ export class PointerHistoryRepository {
                 LEFT JOIN deployments ON pointer_history.deployment = deployments.id
                 WHERE pointer_history.entity_type = $1 AND
                     pointer_history.pointer IN ($2:list) AND
-                    deployments.entity_timestamp >= to_timestamp($3 / 1000.0)
+                    (deployments.entity_timestamp > to_timestamp($3 / 1000.0) OR (deployments.entity_timestamp = to_timestamp($3 / 1000.0) AND deployments.entity_id > $4))
                 ORDER BY deployments.entity_timestamp ASC, deployments.entity_id ASC
                 LIMIT 1`,
-                [entity.type, entity.pointers, entity.timestamp],
+                [entity.type, entity.pointers, entity.timestamp, entity.id],
                 row => row && row.id)
 
             return {
