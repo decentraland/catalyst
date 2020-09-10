cbbcfad9ba120afd7e9da0e3a01aa25ae1ef5f23
Marcos Nuñez Cortes <marcos@decentraland.org>
2020-09-10 18:05:56 -0300

    Added the new Wearable entity type. (#170)

---
+++ content/src/service/access/AccessCheckerForProfiles.ts
@@ -0,0 +1,30 @@
+import { Pointer } from "dcl-catalyst-commons";
+import { EthAddress } from "dcl-crypto";
+import { ContentAuthenticator } from "../auth/Authenticator";
+
+export class AccessCheckerForProfiles {
+
+    constructor(private readonly authenticator: ContentAuthenticator) { }
+
+    public async checkAccess(pointers: Pointer[], ethAddress: EthAddress): Promise<string[]> {
+        const errors: string[] = []
+
+        if (pointers.length != 1) {
+            errors.push(`Only one pointer is allowed when you create a Profile. Received: ${pointers}`)
+        }
+
+        const pointer: Pointer = pointers[0].toLocaleLowerCase()
+
+        if (pointer.startsWith("default")) {
+            if (!this.authenticator.isAddressOwnedByDecentraland(ethAddress)) {
+                errors.push(`Only Decentraland can add or modify default profiles`)
+            }
+        } else if (pointer !== ethAddress.toLocaleLowerCase()) {
+            errors.push(`You can only alter your own profile. The pointer address and the signer address are different.`)
+        }
+
+        return errors
+    }
+
+}
+
