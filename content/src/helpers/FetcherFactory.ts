14196da1f74c0cbd6d86bd830144e197f892a8ce
Agustina Aldasoro <agusaldasoro@gmail.com>
2020-12-11 16:53:54 -0300

    Add user agent header

--- content/src/helpers/FetcherFactory.ts
+++ content/src/helpers/FetcherFactory.ts
@@ -1,10 +1,11 @@
 import { Fetcher } from 'dcl-catalyst-commons'
-import { EnvironmentConfig, Environment } from '../Environment'
+import { EnvironmentConfig, Environment, CURRENT_COMMIT_HASH } from '../Environment'
 
 export class FetcherFactory {
+  private static readonly USER_AGENT_VALUE = `content-server/${CURRENT_COMMIT_HASH} (+https://github.com/decentraland/catalyst)`
+
   static create(env: Environment): Fetcher {
-    const jsonRequestTimeout = env.getConfig<string>(EnvironmentConfig.JSON_REQUEST_TIMEOUT)
-    const fileDownloadTimeout = env.getConfig<string>(EnvironmentConfig.FILE_DOWNLOAD_REQUEST_TIMEOUT)
-    return new Fetcher(jsonRequestTimeout, fileDownloadTimeout)
+    const fetchRequestTimeout = env.getConfig<string>(EnvironmentConfig.FETCH_REQUEST_TIMEOUT)
+    return new Fetcher({ timeout: fetchRequestTimeout, headers: { 'User-Agent': this.USER_AGENT_VALUE } })
   }
 }
