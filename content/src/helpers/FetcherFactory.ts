034c2da73d1e1e9d6dec6d296fddb794a443c757
Agustina Aldasoro <agusaldasoro@gmail.com>
2020-12-14 15:59:53 -0300

    Fix version running

--- content/src/helpers/FetcherFactory.ts
+++ content/src/helpers/FetcherFactory.ts
@@ -2,10 +2,11 @@ import { Fetcher } from 'dcl-catalyst-commons'
 import { EnvironmentConfig, Environment, CURRENT_COMMIT_HASH } from '../Environment'
 
 export class FetcherFactory {
-  private static readonly USER_AGENT_VALUE = `content-server/${CURRENT_COMMIT_HASH} (+https://github.com/decentraland/catalyst)`
-
   static create(env: Environment): Fetcher {
     const fetchRequestTimeout = env.getConfig<string>(EnvironmentConfig.FETCH_REQUEST_TIMEOUT)
-    return new Fetcher({ timeout: fetchRequestTimeout, headers: { 'User-Agent': this.USER_AGENT_VALUE } })
+    return new Fetcher({
+      timeout: fetchRequestTimeout,
+      headers: { 'User-Agent': `content-server/${CURRENT_COMMIT_HASH} (+https://github.com/decentraland/catalyst)` }
+    })
   }
 }
