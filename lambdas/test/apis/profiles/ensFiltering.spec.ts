73815372f5423fba89dbd1a8fcb4b43f48470cad
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-01-06 15:55:34 -0300

    Fixx

--- lambdas/test/apis/profiles/ensFiltering.spec.ts
+++ lambdas/test/apis/profiles/ensFiltering.spec.ts
@@ -1,5 +1,5 @@
 import { Fetcher } from 'dcl-catalyst-commons'
-import { anyString, anything, mock, verify, when } from 'ts-mockito'
+import { anything, instance, mock, verify, when } from 'ts-mockito'
 import { ENSFilter } from '../../../src/apis/profiles/ensFiltering'
 import { DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN } from '../../../src/Environment'
 
@@ -34,76 +34,75 @@ describe('Ensure ENS filtering work as expected', () => {
   }, 100000)
 
   it(`When getting the owned names for the first time, then the graph is consulted`, async () => {
-    const mockedFetcher: Fetcher = mock(Fetcher)
-    when(mockedFetcher.queryGraph(anyString(), anyString(), anything())).thenResolve({
-      nfts: [{ name: 'marcosnc' }]
-    })
+    const mockedFetcher: Fetcher = getMockedFetcher()
+    const fetcher: Fetcher = instance(mockedFetcher)
     const originalAddress = '0x079BED9C31CB772c4C156F86E1CFf15bf751ADd0'
     const ensFilter = new ENSFilter(500, 1000)
 
-    await ensFilter.filter(mockedFetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
+    await ensFilter.filter(fetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
       'marcosnc',
       'invalid_name'
     ])
 
-    verify(mockedFetcher.queryGraph(anything(), anything(), anything(), anything())).once()
+    verify(mockedFetcher.queryGraph(anything(), anything(), anything())).once()
   }, 100000)
 
   it(`When getting the owned names with different listed names, then the graph is consulted`, async () => {
-    const mockedFetcher: Fetcher = mock(Fetcher)
-    when(mockedFetcher.queryGraph(anyString(), anyString(), anything())).thenResolve({
-      nfts: [{ name: 'marcosnc' }]
-    })
+    const mockedFetcher: Fetcher = getMockedFetcher()
+    const fetcher: Fetcher = instance(mockedFetcher)
     const originalAddress = '0x079BED9C31CB772c4C156F86E1CFf15bf751ADd0'
     const ensFilter = new ENSFilter(500, 1000)
 
-    await ensFilter.filter(mockedFetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
+    await ensFilter.filter(fetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
       'marcosnc',
       'invalid_name'
     ])
 
-    await ensFilter.filter(mockedFetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
+    await ensFilter.filter(fetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
       'marcosnc',
       'invalid_name',
       'another_name'
     ])
-    verify(mockedFetcher.queryGraph(anything(), anything(), anything(), anything())).times(2)
+    verify(mockedFetcher.queryGraph(anything(), anything(), anything())).times(2)
   }, 100000)
 
   it(`When getting the owned names twice, then the graph is consulted once`, async () => {
-    const mockedFetcher: Fetcher = mock(Fetcher)
-    when(mockedFetcher.queryGraph(anyString(), anyString(), anything())).thenResolve({
-      nfts: [{ name: 'marcosnc' }]
-    })
+    const mockedFetcher: Fetcher = getMockedFetcher()
+    const fetcher: Fetcher = instance(mockedFetcher)
     const originalAddress = '0x079BED9C31CB772c4C156F86E1CFf15bf751ADd0'
     const ensFilter = new ENSFilter(500, 1000)
 
-    await ensFilter.filter(mockedFetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
+    await ensFilter.filter(fetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
       'marcosnc',
       'invalid_name'
     ])
-    await ensFilter.filter(mockedFetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
+    await ensFilter.filter(fetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
       'marcosnc',
       'invalid_name'
     ])
 
-    verify(mockedFetcher.queryGraph(anything(), anything(), anything(), anything())).once()
+    verify(mockedFetcher.queryGraph(anything(), anything(), anything())).once()
   }, 100000)
 
   it(`When getting the owned names for a different entity, then the graph is consulted twice`, async () => {
-    const mockedFetcher: Fetcher = mock(Fetcher)
-    when(mockedFetcher.queryGraph(anyString(), anyString(), anything())).thenResolve({
-      nfts: [{ name: 'marcosnc' }]
-    })
+    const mockedFetcher: Fetcher = getMockedFetcher()
+    const fetcher: Fetcher = instance(mockedFetcher)
     const originalAddress = '0x079BED9C31CB772c4C156F86E1CFf15bf751ADd0'
     const ensFilter = new ENSFilter(500, 1000)
 
-    await ensFilter.filter(mockedFetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
+    await ensFilter.filter(fetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, originalAddress, [
       'marcosnc',
       'invalid_name'
     ])
 
-    await ensFilter.filter(mockedFetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, 'anotherAddress', ['marcosnc'])
-    verify(mockedFetcher.queryGraph(anything(), anything(), anything(), anything())).times(2)
+    await ensFilter.filter(fetcher, DEFAULT_ENS_OWNER_PROVIDER_URL_ROPSTEN, 'anotherAddress', ['marcosnc'])
+    verify(mockedFetcher.queryGraph(anything(), anything(), anything())).times(2)
   }, 100000)
 })
+function getMockedFetcher() {
+  const mockedFetcher: Fetcher = mock(Fetcher)
+  when(mockedFetcher.queryGraph(anything(), anything(), anything())).thenResolve({
+    nfts: [{ name: 'marcosnc' }]
+  })
+  return mockedFetcher
+}
