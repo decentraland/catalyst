d11d8b5857585cc175f9a08faee4cd7b2c09daf7
Nicolas Chamo <nicolas@chamo.com.ar>
2021-02-09 09:47:04 -0300

    feat: adding wearables by owner endpoint (#280)

--- lambdas/src/apis/collections/routes.ts
+++ lambdas/src/apis/collections/routes.ts
@@ -1,11 +1,18 @@
 import { SmartContentClient } from '@katalyst/lambdas/utils/SmartContentClient'
+import { TheGraphClient } from '@katalyst/lambdas/utils/TheGraphClient'
 import { Request, Response, Router } from 'express'
 import { contentsImage, contentsThumbnail, getStandardErc721 } from './controllers/collections'
+import { getWearablesByOwnerEndpoint as getWearablesByOwnerHandler } from './controllers/wearables'
 
-export function initializeCollectionsRoutes(router: Router, client: SmartContentClient): Router {
+export function initializeCollectionsRoutes(
+  router: Router,
+  client: SmartContentClient,
+  theGraphClient: TheGraphClient
+): Router {
   router.get('/standard/erc721/:chainId/:contract/:option/:emission?', createHandler(client, getStandardErc721))
   router.get('/contents/:urn/image', createHandler(client, contentsImage))
   router.get('/contents/:urn/thumbnail', createHandler(client, contentsThumbnail))
+  router.get('/wearables-by-owner/:owner', (req, res) => getWearablesByOwnerHandler(client, theGraphClient, req, res))
   return router
 }
 
