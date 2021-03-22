93409b5c2c152b5e7ce3cb0dceec8e9acb835519
Nicolas Chamo <nicolas@chamo.com.ar>
2021-03-22 17:43:54 -0300

    feat: add origin information to the client (#432)

--- content/src/helpers/FetcherFactory.ts
+++ content/src/helpers/FetcherFactory.ts
@@ -4,9 +4,13 @@ import { CURRENT_COMMIT_HASH, Environment, EnvironmentConfig } from '../Environm
 export class FetcherFactory {
   static create(env: Environment): Fetcher {
     const fetchRequestTimeout = env.getConfig<string>(EnvironmentConfig.FETCH_REQUEST_TIMEOUT)
+    const contentServerAddress = env.getConfig<string>(EnvironmentConfig.CONTENT_SERVER_ADDRESS)
     return new Fetcher({
       timeout: fetchRequestTimeout,
-      headers: { 'User-Agent': `content-server/${CURRENT_COMMIT_HASH} (+https://github.com/decentraland/catalyst)` }
+      headers: {
+        'User-Agent': `content-server/${CURRENT_COMMIT_HASH} (+https://github.com/decentraland/catalyst)`,
+        Origin: contentServerAddress
+      }
     })
   }
 }
