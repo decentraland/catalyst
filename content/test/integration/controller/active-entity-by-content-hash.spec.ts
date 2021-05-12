3d0d2367ffe5e8b39a4c600310de5aaf673dae4f
jmoguilevsky <jmoguilevsky@gmail.com>
2021-05-12 17:05:01 -0300

    added integration test

--- content/test/integration/controller/active-entity-by-content-hash.spec.ts
+++ content/test/integration/controller/active-entity-by-content-hash.spec.ts
@@ -1,14 +1,16 @@
 import { Bean } from '@katalyst/content/Environment'
+import { EntityByHash } from '@katalyst/content/service/deployments/DeploymentManager'
 import { MockedSynchronizationManager } from '@katalyst/test-helpers/service/synchronization/MockedSynchronizationManager'
-// import { Fetcher } from 'dcl-catalyst-commons'
+import { Fetcher } from 'dcl-catalyst-commons'
 import fetch from 'node-fetch'
 import { loadStandaloneTestEnvironment } from '../E2ETestEnvironment'
+import { buildDeployData } from '../E2ETestUtils'
 import { TestServer } from '../TestServer'
 
 fdescribe('Integration - Get Active Entity By Content Hash', () => {
   const testEnv = loadStandaloneTestEnvironment()
   let server: TestServer
-  // const fetcher = new Fetcher()
+  const fetcher = new Fetcher()
 
   beforeEach(async () => {
     server = await testEnv
@@ -28,48 +30,23 @@ fdescribe('Integration - Get Active Entity By Content Hash', () => {
     expect(body.error).toBe('The entity was not found')
   })
 
-  // it('When the deployment exists returns the entity id', async () => {
-  //   const { deployData } = await buildDeployData(['0,0', '0,1'], {
-  //     metadata: 'this is just some metadata',
-  //     contentPaths: ['content/test/integration/resources/some-binary-file.png']
-  //   })
+  it('When the deployment exists returns the entity id', async () => {
+    const deployResult = await buildDeployData(['0,0', '0,1'], {
+      metadata: 'this is just some metadata',
+      contentPaths: ['content/test/integration/resources/some-binary-file.png']
+    })
 
-  //   // Deploy entity
-  //   await server.deploy(deployData)
+    // Deploy entity
+    await server.deploy(deployResult.deployData)
 
-  //   await fetchActiveEntity("asd")
-  //   // exp
-  // })
-  // const testEnv = loadStandaloneTestEnvironment()
-  // let server: TestServer
-  // const fetcher = new Fetcher()
+    const result = await fetchActiveEntity(deployResult.entity.content?.get('some-binary-file.png') || '')
 
-  // beforeAll(async () => {
-  //   server = await testEnv
-  //     .configServer()
-  //     .withBean(Bean.SYNCHRONIZATION_MANAGER, new MockedSynchronizationManager())
-  //     .andBuild()
-  //   await server.start()
-  // })
+    expect(result?.entityId).toEqual(deployResult.entity.id)
+  })
 
-  // it(`When garbage collection is on, then unused content is deleted`, async () => {
-  //   const { deployData } = await buildDeployData(['0,0', '0,1'], {
-  //     metadata: 'this is just some metadata',
-  //     contentPaths: ['content/test/integration/resources/some-binary-file.png']
-  //   })
-
-  //   // Deploy entity
-  //   await server.deploy(deployData)
-
-  //   // Fetch deployments
-  //   await fetchActiveEntity("")
-
-  //   expect(true).toBe(false);
-  // })
-
-  // async function fetchActiveEntity(contentHash: string): Promise<EntityByHash> {
-  //   const url = server.getAddress() + `/contents/${contentHash}/active-entity`
-  //   console.log(url)
-  //   return fetcher.fetchJson(url)
-  // }
+  async function fetchActiveEntity(contentHash: string): Promise<EntityByHash> {
+    const url = server.getAddress() + `/contents/${contentHash}/active-entity`
+    console.log(url)
+    return fetcher.fetchJson(url)
+  }
 })
