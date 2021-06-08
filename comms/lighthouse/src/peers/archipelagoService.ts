c9223495476c3beda1bb155f7773848df3b092c9
Nicolas Chamo <nicolas@chamo.com.ar>
2021-06-08 12:11:26 -0300

    First commit

--- comms/lighthouse/src/peers/archipelagoService.ts
+++ comms/lighthouse/src/peers/archipelagoService.ts
@@ -1,11 +1,24 @@
-import { ArchipelagoController, ArchipelagoControllerOptions, defaultArchipelagoController } from '@dcl/archipelago'
+import { ArchipelagoController, defaultArchipelagoController } from '@dcl/archipelago'
 import { isPosition3D, Position } from 'decentraland-catalyst-utils/Positions'
+import { ConfigService, LighthouseConfig } from '../config/configService'
 
 export class ArchipelagoService {
   private readonly controller: ArchipelagoController
 
-  constructor(options: ArchipelagoControllerOptions) {
-    this.controller = defaultArchipelagoController(options)
+  constructor({ configService }: { configService: ConfigService }) {
+    this.controller = defaultArchipelagoController({
+      archipelagoParameters: {
+        joinDistance: configService.get(LighthouseConfig.ARCHIPELAGO_JOIN_DISTANCE),
+        leaveDistance: configService.get(LighthouseConfig.ARCHIPELAGO_LEAVE_DISTANCE)
+      }
+    })
+
+    configService.listenTo(LighthouseConfig.ARCHIPELAGO_JOIN_DISTANCE, (joinDistance) =>
+      this.controller.modifyOptions({ joinDistance })
+    )
+    configService.listenTo(LighthouseConfig.ARCHIPELAGO_LEAVE_DISTANCE, (leaveDistance) =>
+      this.controller.modifyOptions({ leaveDistance })
+    )
   }
 
   updatePeerPosition(peerId: string, position?: Position) {
