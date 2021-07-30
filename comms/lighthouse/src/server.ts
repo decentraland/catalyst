ee151c34557935c851491e878e39463b8cd0637d
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-07-30 16:44:43 -0300

    Fix typo

--- comms/lighthouse/src/server.ts
+++ comms/lighthouse/src/server.ts
@@ -63,7 +63,7 @@ const CURRENT_ETH_NETWORK = process.env.ETH_NETWORK ?? DEFAULT_ETH_NETWORK
   const corsOptions: cors.CorsOptions = {
     origin: true,
     methods: 'GET,HEAD,POST,PUT,DELETE,CONNECT,TRACE,PATCH',
-    allowedHeaders: ['X-Peer-Token', 'content-type'],
+    allowedHeaders: ['X-Peer-Token', 'Content-Type'],
     credentials: true
   }
 
