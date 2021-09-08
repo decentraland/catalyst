f7a842d27d4c95d544b061ebe0f3b8ba49509bc8
guidota <guido@decentraland.org>
2021-09-08 17:14:03 -0300

    chore: remove unused comms/performance-test code

--- comms/performance-test/client/protobuf/compile_proto.sh
+++
@@ -1,5 +0,0 @@
-protoc --plugin=../../../../node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. ./comms.proto
-
-sed -i 's/global\.//g' comms.ts
-
-sed -i 's:message\.dst\.push(e);://@ts-ignore\n        message.dst.push(e);:g' comms.ts
