4e5d30d91e98709d824a8e4b2ccf5ac3bc4cbfa1
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-09-16 16:07:39 -0300

    Add more time

--- content/src/Environment.ts
+++ content/src/Environment.ts
@@ -227,7 +227,7 @@ export class EnvironmentBuilder {
     this.registerConfigIfNotAlreadySet(
       env,
       EnvironmentConfig.CHECK_SYNC_RANGE,
-      () => process.env.CHECK_SYNC_RANGE ?? ms('1m')
+      () => process.env.CHECK_SYNC_RANGE ?? ms('30m')
     )
     this.registerConfigIfNotAlreadySet(env, EnvironmentConfig.DECENTRALAND_ADDRESS, () => DECENTRALAND_ADDRESS)
     this.registerConfigIfNotAlreadySet(
