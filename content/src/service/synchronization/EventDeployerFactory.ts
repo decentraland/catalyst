eed7cc4b0bac66b970fb9421ac57945b8df8ad0d
jmoguilevsky <jmoguilevsky@gmail.com>
2021-09-20 12:34:30 -0300

    Add stream timeout (#656)
    * added timeout
    
    * remove log
    
    * pr review
    
    * remove unused code
    
    * added logging and ignore the timeout in the case it's bootstrapping
    
    * missing change
    
    * minor fix

--- content/src/service/synchronization/EventDeployerFactory.ts
+++ content/src/service/synchronization/EventDeployerFactory.ts
@@ -1,8 +1,12 @@
-import { Bean, Environment } from '../../Environment'
+import { Bean, Environment, EnvironmentConfig } from '../../Environment'
 import { EventDeployer } from './EventDeployer'
 
 export class EventDeployerFactory {
   static create(env: Environment): EventDeployer {
-    return new EventDeployer(env.getBean(Bean.CONTENT_CLUSTER), env.getBean(Bean.SERVICE))
+    return new EventDeployer(
+      env.getBean(Bean.CONTENT_CLUSTER),
+      env.getBean(Bean.SERVICE),
+      env.getConfig(EnvironmentConfig.SYNC_STREAM_TIMEOUT)
+    )
   }
 }
