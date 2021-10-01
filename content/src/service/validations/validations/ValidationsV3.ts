761fbdf5ac895401b3cb2d81c0becb29577e51f5
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-10-01 14:49:45 -0300

    Remove rate limit when syncing catalysts

--- content/src/service/validations/validations/ValidationsV3.ts
+++ content/src/service/validations/validations/ValidationsV3.ts
@@ -25,8 +25,7 @@ export const VALIDATIONS_V3: ValidationsForContext = {
     Validations.NO_REDEPLOYS,
     Validations.LEGACY_ENTITY,
     Validations.CONTENT_V3,
-    Validations.DECENTRALAND_ADDRESS,
-    Validations.RATE_LIMIT
+    Validations.DECENTRALAND_ADDRESS
   ],
   // This is a context during synchronization: when the deployment is already deployed.
   // That's why here it's not present NO_REDEPLOY, during sync you can receive the same deployment from different catalysts.
@@ -35,8 +34,7 @@ export const VALIDATIONS_V3: ValidationsForContext = {
     Validations.SIGNATURE,
     Validations.ACCESS,
     Validations.ENTITY_STRUCTURE,
-    Validations.CONTENT_V3,
-    Validations.RATE_LIMIT
+    Validations.CONTENT_V3
   ],
   [DeploymentContext.SYNCED_LEGACY_ENTITY]: [
     // TODO: Add limit so that v3 entities can only be deployed up to a certain date
@@ -44,8 +42,7 @@ export const VALIDATIONS_V3: ValidationsForContext = {
     Validations.ENTITY_STRUCTURE,
     Validations.LEGACY_ENTITY,
     Validations.CONTENT_V3,
-    Validations.DECENTRALAND_ADDRESS,
-    Validations.RATE_LIMIT
+    Validations.DECENTRALAND_ADDRESS
   ],
   // This is during synchronization when a deployment needs to  be done, but you already have a newer which overwrites it.
   // So, at this moment the files from the entity of the overwritten deployment are not download.
@@ -53,16 +50,14 @@ export const VALIDATIONS_V3: ValidationsForContext = {
     // TODO: Add limit so that v3 entities can only be deployed up to a certain date
     Validations.SIGNATURE,
     Validations.ACCESS,
-    Validations.ENTITY_STRUCTURE,
-    Validations.RATE_LIMIT
+    Validations.ENTITY_STRUCTURE
   ],
   [DeploymentContext.OVERWRITTEN_LEGACY_ENTITY]: [
     // TODO: Add limit so that v3 entities can only be deployed up to a certain date
     Validations.SIGNATURE,
     Validations.ENTITY_STRUCTURE,
     Validations.LEGACY_ENTITY,
-    Validations.DECENTRALAND_ADDRESS,
-    Validations.RATE_LIMIT
+    Validations.DECENTRALAND_ADDRESS
   ],
   // This context is used only when running the fix deployments script
   [DeploymentContext.FIX_ATTEMPT]: [
