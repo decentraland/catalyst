37fcf2d50b99adffec3faadbbe4ec6fca27779c1
guidota <guido@decentraland.org>
2021-11-12 10:13:17 -0300

    fix: add missing metric properties

--- lambdas/src/utils/files.ts
+++ lambdas/src/utils/files.ts
@@ -4,3 +4,10 @@ export async function getFileStream(filePath: string): Promise<[NodeJS.ReadableS
   const stat = await fs.promises.stat(filePath)
   return [fs.createReadStream(filePath), stat.size]
 }
+
+export async function checkFileExists(file: string): Promise<boolean> {
+  return fs.promises
+    .access(file, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false)
+}
