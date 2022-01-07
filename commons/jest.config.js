22ae9f348beba45077aeaa2e2ef2b685343acd6d
Juan Scolari <jscolari@decentraland.org>
2022-01-07 08:30:43 -0300

    test: silence console in tests

--- commons/jest.config.js
+++ commons/jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  silent: true,
   testEnvironment: 'node',
   testMatch: ["**/*.spec.(ts)"],
   verbose: true,
