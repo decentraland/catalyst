d9c28db43500035fc15305f9bbb75f733faf50f0
Juan Scolari <jscolari@decentraland.org>
2022-01-10 13:46:40 -0300

    test: fix linter errors

--- comms/lighthouse/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    silent: true,
-    testEnvironment: 'node',
-    testMatch: ["**/*.spec.(ts)"],
-    verbose: true,
-  };
