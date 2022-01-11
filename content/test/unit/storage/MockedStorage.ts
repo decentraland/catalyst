502e3e904103c788bc32ebb7ca677ffc600c0609
guidota <guido@decentraland.org>
2022-01-10 21:53:37 -0300

    test: fix storeContent method in MockedStorage

--- content/test/unit/storage/MockedStorage.ts
+++ content/test/unit/storage/MockedStorage.ts
@@ -8,8 +8,8 @@ export class MockedStorage implements ContentStorage {
     const buffer = this.storage.get(fileHash)
     return Promise.resolve(buffer?.byteLength)
   }
-  storeContent(fileHash: string, content: Uint8Array | Readable) {
-    this.storage.set(id, content instanceof Uint8Array ? content : await streamToBuffer(content))
+  async storeContent(fileHash: string, content: Uint8Array | Readable) {
+    this.storage.set(fileHash, content instanceof Uint8Array ? content : await streamToBuffer(content))
   }
   async storeStream(id: string, content: Readable): Promise<void> {
     this.storage.set(id, await streamToBuffer(content))
