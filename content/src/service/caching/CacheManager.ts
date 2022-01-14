2f97a9ea2dc6b44aace7bb8e52ee41678a669a8b
guidota <guido@decentraland.org>
2022-01-14 15:00:24 -0300

    chore: bump `@dcl/content-validator` version

--- content/src/service/caching/CacheManager.ts
+++ content/src/service/caching/CacheManager.ts
@@ -57,7 +57,9 @@ class CacheConfigBuilder {
   build(): CacheConfig {
     Object.values(EntityType).forEach((entityType: EntityType) => {
       if (!this.defaultSizes.has(entityType)) {
-        throw new Error(`Can't build a cache config since it is missing the type '${entityType}'.`)
+        // fill default size for missing type with default value (2000)
+        // making not required for new entities to define a default size
+        this.defaultSizes.set(entityType, 2000)
       }
     })
     return new CacheConfig(this)
