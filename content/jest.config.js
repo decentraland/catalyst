b7974a640e952087c4609dc11d754128bc8e7a02
guidota <guido@decentraland.org>
2022-01-18 14:29:01 -0300

    chore(ci): merge coverage reports of parallel executions

--- content/jest.config.js
+++ content/jest.config.js
@@ -1,19 +1,23 @@
 module.exports = {
   coverageDirectory: 'coverage',
+  coverageReporters: ['json'],
   collectCoverageFrom: ['./src/**/*.ts'],
   testEnvironment: 'node',
   testTimeout: 60000,
   verbose: true,
-  projects: [{
-    displayName: 'unit',
-    testMatch: ['**/test/unit/**/*.spec.(ts)'],
-    preset: 'ts-jest',
-  }, {
-    displayName: 'integration',
-    testMatch: ['**/test/integration/**/*.spec.(ts)'],
-    globalSetup: './jest.globalSetup.ts',
-    globalTeardown: './jest.globalTeardown.ts',
-    setupFilesAfterEnv: ['./jest.setupFilesAfterEnv.ts'],
-    preset: 'ts-jest',
-  }]
-};
+  projects: [
+    {
+      displayName: 'unit',
+      testMatch: ['**/test/unit/**/*.spec.(ts)'],
+      preset: 'ts-jest'
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['**/test/integration/**/*.spec.(ts)'],
+      globalSetup: './jest.globalSetup.ts',
+      globalTeardown: './jest.globalTeardown.ts',
+      setupFilesAfterEnv: ['./jest.setupFilesAfterEnv.ts'],
+      preset: 'ts-jest'
+    }
+  ]
+}
