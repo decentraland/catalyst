652e509b58ca8920898ddc39d09462213b3b73c4
menduz <github@menduz.com>
2022-01-26 19:45:02 -0300

    use both jest reporters in ci

--- content/test/unit/storage/MockedStorage.ts
+++ content/test/unit/storage/MockedStorage.ts
@@ -4,20 +4,23 @@ import { ContentItem, ContentStorage, SimpleContentItem, streamToBuffer } from '
 export class MockedStorage implements ContentStorage {
   private storage: Map<string, Uint8Array> = new Map()
 
+  async storeStreamAndCompress(fileId: string, content: Readable): Promise<void> {
+    this.storage.set(fileId, await streamToBuffer(content))
+  }
   async exist(fileId: string): Promise<boolean> {
     return this.storage.has(fileId)
   }
-  async storeStream(id: string, content: Readable): Promise<void> {
-    this.storage.set(id, await streamToBuffer(content))
+  async storeStream(fileId: string, content: Readable): Promise<void> {
+    this.storage.set(fileId, await streamToBuffer(content))
   }
   async delete(ids: string[]): Promise<void> {
     ids.forEach((id) => this.storage.delete(id))
   }
-  async retrieve(id: string): Promise<ContentItem | undefined> {
-    const content = this.storage.get(id)
+  async retrieve(fileId: string): Promise<ContentItem | undefined> {
+    const content = this.storage.get(fileId)
     return content ? SimpleContentItem.fromBuffer(content) : undefined
   }
-  async existMultiple(ids: string[]): Promise<Map<string, boolean>> {
-    return new Map(ids.map((id) => [id, this.storage.has(id)]))
+  async existMultiple(fileIds: string[]): Promise<Map<string, boolean>> {
+    return new Map(fileIds.map((fileId) => [fileId, this.storage.has(fileId)]))
   }
 }
