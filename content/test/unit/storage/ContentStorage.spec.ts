be5f5c2eabc1dcd6c65acf529ad4e20367a9b8d4
Pedro Tamborindeguy <pftambo@gmail.com>
2022-01-28 13:16:37 -0300

    feat: add sharding for stored files

--- content/test/unit/storage/ContentStorage.spec.ts
+++ content/test/unit/storage/ContentStorage.spec.ts
@@ -9,6 +9,8 @@ describe('ContentStorage', () => {
   let storage: ContentStorage
   let id: string
   let content: Buffer
+  let id2: string
+  let content2: Buffer
 
   beforeAll(async () => {
     env = new Environment()
@@ -17,6 +19,8 @@ describe('ContentStorage', () => {
 
     id = 'some-id'
     content = Buffer.from('123')
+    id2 = 'another-id'
+    content2 = Buffer.from('456')
   })
 
   describe('Buffer utils', () => {
@@ -51,9 +55,7 @@ describe('ContentStorage', () => {
   it(`When content is stored, then it can be retrieved`, async () => {
     await storage.storeStream(id, bufferToStream(content))
 
-    const retrievedContent = await storage.retrieve(id)
-
-    expect(await streamToBuffer(await retrievedContent!.asStream())).toEqual(content)
+    await retrieveAndExpectStoredContentToBe(id, content)
   })
 
   it(`When content is stored, then we can check if it exists`, async function () {
@@ -71,8 +73,7 @@ describe('ContentStorage', () => {
     await storage.storeStream(id, bufferToStream(content))
     await storage.storeStream(id, bufferToStream(newContent))
 
-    const retrievedContent = await storage.retrieve(id)
-    expect(await streamToBuffer(await retrievedContent!.asStream())).toEqual(newContent)
+    await retrieveAndExpectStoredContentToBe(id, newContent)
   })
 
   it(`When content is deleted, then it is no longer available`, async function () {
@@ -88,4 +89,50 @@ describe('ContentStorage', () => {
     expect(await storage.exist(id)).toBe(false)
     expect(exists.get(id)).toBe(false)
   })
+
+  it(`When multiple content is stored, then multiple content exist`, async () => {
+    await storage.storeStream(id, bufferToStream(content))
+    await storage.storeStream(id2, bufferToStream(content2))
+    expect(await storage.existMultiple([id, id2, 'notStored'])).toEqual(
+      new Map([[id, true], [id2, true], ['notStored', false]])
+    )
+})
+
+  it(`When multiple content is stored, then multiple content is correct`, async () => {
+    await storage.storeStream(id, bufferToStream(content))
+    await storage.storeStream(id2, bufferToStream(content2))
+
+    await retrieveAndExpectStoredContentToBe(id, content)
+    await retrieveAndExpectStoredContentToBe(id2, content2)
+  })
+
+  it(`When content is deleted, then the correct contented is deleted`, async () => {
+    await storage.storeStream(id, bufferToStream(content))
+    await storage.storeStream(id2, bufferToStream(content2))
+    await storage.delete([id2])
+    expect(await storage.exist(id)).toBeTruthy()
+    expect(await storage.exist(id2)).toBeFalsy()
+    await retrieveAndExpectStoredContentToBe(id, content)
+  })
+
+  it(`When a content with bad compression ratio is stored and compressed, then it is not stored compressed`, async () => {
+    await storage.storeStreamAndCompress(id, bufferToStream(content))
+    const retrievedContent = (await storage.retrieve(id))
+    expect((await retrievedContent!.asRawStream()).encoding).toBeNull()
+    expect(await streamToBuffer(await retrievedContent!.asStream())).toEqual(content)
+
+  })
+
+  it(`When a content with good compression ratio is stored and compressed, then it is stored compressed`, async () => {
+    const goodCompresstionRatioContent = Buffer.from(new Uint8Array(100).fill(0))
+    await storage.storeStreamAndCompress(id, bufferToStream(goodCompresstionRatioContent))
+    const retrievedContent = (await storage.retrieve(id))
+    expect((await retrievedContent!.asRawStream()).encoding).toBe('gzip')
+    expect(await streamToBuffer(await retrievedContent!.asStream())).toEqual(goodCompresstionRatioContent)
+  })
+
+  async function retrieveAndExpectStoredContentToBe(idToRetrieve: string, expectedContent: Buffer) {
+    const retrievedContent = await storage.retrieve(idToRetrieve)
+    expect(await streamToBuffer(await retrievedContent!.asStream())).toEqual(expectedContent)
+  }
 })
