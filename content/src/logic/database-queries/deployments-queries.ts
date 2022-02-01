b5e695eac9c2dd799f1069b3072a918b1153c1c9
Agustina Aldasoro <agusaldasoro@gmail.com>
2022-02-01 10:00:01 -0300

    Undo reuse query

--- content/src/logic/database-queries/deployments-queries.ts
+++ content/src/logic/database-queries/deployments-queries.ts
@@ -82,28 +82,38 @@ export function getHistoricalDeploymentsQuery(
   const order: string = sorting.order
 
   // Generate the select according the info needed
-  let select: string = `
-              SELECT
-                  dep1.id,
-                  dep1.entity_type,
-                  dep1.entity_id,
-                  dep1.entity_pointers,
-                  date_part('epoch', dep1.entity_timestamp) * 1000 AS entity_timestamp,
-                  dep1.entity_metadata,
-                  dep1.deployer_address,
-                  dep1.version,
-                  dep1.auth_chain,
-                  date_part('epoch', dep1.local_timestamp) * 1000 AS local_timestamp`
+  let query: SQLStatement
   if (!!filters?.includeOverwrittenInfo) {
-    select += `,
-                  dep2.entity_id AS overwritten_by
-              FROM deployments AS dep1
-              LEFT JOIN deployments AS dep2 ON dep1.deleter_deployment = dep2.id`
+    query = SQL`
+            SELECT
+                dep1.id,
+                dep1.entity_type,
+                dep1.entity_id,
+                dep1.entity_pointers,
+                date_part('epoch', dep1.entity_timestamp) * 1000 AS entity_timestamp,
+                dep1.entity_metadata,
+                dep1.deployer_address,
+                dep1.version,
+                dep1.auth_chain,
+                date_part('epoch', dep1.local_timestamp) * 1000 AS local_timestamp,
+                dep2.entity_id AS overwritten_by
+            FROM deployments AS dep1
+            LEFT JOIN deployments AS dep2 ON dep1.deleter_deployment = dep2.id`
   } else {
-    select += `
-              FROM deployments AS dep1`
+    query = SQL`
+            SELECT
+                dep1.id,
+                dep1.entity_type,
+                dep1.entity_id,
+                dep1.entity_pointers,
+                date_part('epoch', dep1.entity_timestamp) * 1000 AS entity_timestamp,
+                dep1.entity_metadata,
+                dep1.deployer_address,
+                dep1.version,
+                dep1.auth_chain,
+                date_part('epoch', dep1.local_timestamp) * 1000 AS local_timestamp,
+            FROM deployments AS dep1`
   }
-  const query = SQL(select)
 
   const whereClause: SQLStatement[] = []
   // Configure sort and order
