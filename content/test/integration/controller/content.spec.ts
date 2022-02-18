d26f4630dd459aa77b1d8f999969bab15eb3d7ac
Juan Scolari <jscolari@decentraland.org>
2022-02-18 20:35:01 -0300

    fix: use headContent controller for HEAD contents endpoint

---
+++ content/test/integration/controller/content.spec.ts
@@ -0,0 +1,47 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+import path from 'path'
+import { Controller } from '../../../src/controller/Controller'
+import { EnvironmentConfig } from '../../../src/Environment'
+import { FileSystemContentStorage } from '../../../src/storage/FileSystemContentStorage'
+import { makeNoopValidator } from '../../helpers/service/validations/NoOpValidator'
+import { loadStandaloneTestEnvironment } from '../E2ETestEnvironment'
+
+loadStandaloneTestEnvironment()('Integration - Get Content', (testEnv) => {
+  let testFileText: string
+  let getFilePathSpy: jest.SpyInstance
+
+  beforeAll(async () => {
+    const testFilePath = path.resolve(__dirname, '../', 'resources', 'some-text-file.txt')
+    testFileText = (await fs.promises.readFile(testFilePath)).toString()
+
+    getFilePathSpy = jest.spyOn(FileSystemContentStorage.prototype as any, 'getFilePath')
+    getFilePathSpy.mockReturnValue(testFilePath)
+  })
+
+  afterAll(() => {
+    getFilePathSpy.mockRestore()
+  })
+
+  it('calls the headContent controller when the head endpoint is requested', async () => {
+    const headContentSpy = jest.spyOn(Controller.prototype, 'headContent')
+    const getContentSpy = jest.spyOn(Controller.prototype, 'getContent')
+
+    const server = await testEnv.configServer().withConfig(EnvironmentConfig.DISABLE_SYNCHRONIZATION, true).andBuild()
+
+    makeNoopValidator(server.components)
+
+    await server.startProgram()
+
+    const url = server.getUrl() + `/contents/QmNn5Zs21mZeYitXTQyjVyVaThvzjKUVFgvBjXxJPAevdU`
+    const res = await fetch(url, { method: 'HEAD' })
+
+    let text = (await res.buffer()).toString()
+
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-length')).toBe(testFileText.length.toString())
+
+    expect(headContentSpy).toHaveBeenCalledTimes(1)
+    expect(getContentSpy).toHaveBeenCalledTimes(0)
+  })
+})
