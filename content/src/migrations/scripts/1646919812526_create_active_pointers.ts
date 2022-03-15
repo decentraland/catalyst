762b1f87ca6625003c6f6ec81125151df6da05eb
Agustina Aldasoro <agusaldasoro@gmail.com>
2022-03-15 19:01:43 -0300

    Add migration

--- content/src/migrations/scripts/1646919812526_create_active_pointers.ts
+++ content/src/migrations/scripts/1646919812526_create_active_pointers.ts
@@ -5,6 +5,13 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
     pointer: { type: 'text', primaryKey: true },
     entity_id: { type: 'text', notNull: true }
   })
+
+  pgm.sql(`
+    INSERT INTO active_pointers (pointer, entity_id)
+    SELECT UNNEST(entity_pointers) as pointer, entity_id
+    FROM deployments
+    WHERE deleter_deployment IS NULL;
+  `)
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
