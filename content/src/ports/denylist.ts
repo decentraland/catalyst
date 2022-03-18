853ec5b9206f3586bc0b534d254956054931b47d
Pedro Tamborindeguy <pftambo@gmail.com>
2022-03-18 15:05:33 -0300

    refactor: refactor denylist and added tests

--- content/src/ports/denylist.ts
+++ content/src/ports/denylist.ts
@@ -1,128 +1,104 @@
-import { IBaseComponent, ILoggerComponent } from '@well-known-components/interfaces'
+import { IBaseComponent } from '@well-known-components/interfaces'
 import { resolve } from 'path'
 import { createInterface } from 'readline'
 import { URL } from 'url'
 import { EnvironmentConfig } from '../Environment'
 import { AppComponents } from '../types'
+import { FSComponent } from './fs'
 
-export interface DenylistComponent {
-  isDenyListed(hash: string): boolean
+export interface Denylist {
+  isDenylisted(id: string): boolean
 }
 
-type DenylistAdd = {
-  add(cid: string): void
-  clear(): void
-}
-
-async function loadDenyListFromFile(
-  logger: ILoggerComponent.ILogger,
-  components: Pick<AppComponents, 'fs'>,
-  fileName: string,
-  deniedContentIdentifiers: DenylistAdd
-) {
-  if (!fileName) return
-
-  if (!(await components.fs.existPath(fileName))) {
-    logger.error("Denylist file doesn't exist", { fileName })
-    return
+export async function createDenylist(
+  components: Pick<AppComponents, 'env' | 'logs' | 'fetcher'> & {
+    fs: Pick<FSComponent, 'createReadStream' | 'existPath'>
   }
-
-  const content = components.fs.createReadStream(fileName, {
-    encoding: 'utf-8'
-  })
-
-  const lines = createInterface({
-    input: content,
-    crlfDelay: Infinity
-  })
-
-  await processLines(logger, deniedContentIdentifiers, lines)
-}
-
-async function processLines(
-  logger: ILoggerComponent.ILogger,
-  deniedContentIdentifiers: DenylistAdd,
-  lines: Iterable<string> | AsyncIterable<string>
-) {
-  for await (const line of lines) {
-    try {
-      const cid = line.trim()
-      if (!cid.startsWith('#')) {
-        deniedContentIdentifiers.add((line as string).trim())
-      }
-    } catch (err: any) {
-      logger.error(err)
-    }
-  }
-}
-
-async function loadDenyListFromUrl(
-  logger: ILoggerComponent.ILogger,
-  components: Pick<AppComponents, 'fetcher'>,
-  downloadableLink: string,
-  deniedContentIdentifiers: DenylistAdd
-) {
-  if (!downloadableLink) return
-
-  try {
-    const url = new URL(downloadableLink)
-    const response = await components.fetcher.fetch(url.toString())
-    const content = await response.text()
-    const lines = content.split(/[\r\n\s]+/)
-    await processLines(logger, deniedContentIdentifiers, lines)
-  } catch (err: any) {
-    logger.error(err)
-  }
-}
-
-async function reloadDenyLists(
-  logger: ILoggerComponent.ILogger,
-  components: Pick<AppComponents, 'env' | 'fs' | 'fetcher'>,
-  deniedContentIdentifiers: DenylistAdd
-) {
+): Promise<Denylist & IBaseComponent> {
+  const logger = components.logs.getLogger('Denylist')
+  const deniedContentIdentifiers = new Set()
   const fileName = resolve(
     components.env.getConfig(EnvironmentConfig.STORAGE_ROOT_FOLDER),
     components.env.getConfig(EnvironmentConfig.DENYLIST_FILE_NAME)
   )
 
-  components.env.getConfig(EnvironmentConfig.DENYLIST_FILE_NAME)
+  const denylistsUrlsRaw = components.env.getConfig<string>(EnvironmentConfig.DENYLIST_URLS)?.split(/[\r\n\s]+/gm) ?? []
+  const denylistsUrls: URL[] = denylistsUrlsRaw
+    .filter((url) => {
+      try {
+        new URL(url)
+        return true
+      } catch {
+        return false
+      }
+    })
+    .map((url) => new URL(url))
+  logger.info(`File: ${fileName}`)
+  logger.info(`Urls: ${denylistsUrls}`)
 
-  try {
-    await loadDenyListFromFile(logger, components, fileName, deniedContentIdentifiers)
+  const processLines = async (lines: Iterable<string> | AsyncIterable<string>) => {
+    for await (const line of lines) {
+      try {
+        const cid = line.trim()
+        if (!cid.startsWith('#')) {
+          deniedContentIdentifiers.add((line as string).trim())
+        }
+      } catch (err: any) {
+        logger.error(err)
+      }
+    }
+  }
 
-    const denylistsUrls = components.env.getConfig<string>(EnvironmentConfig.DENYLIST_URLS)?.split(/[\r\n\s]+/gm)
-
-    for (const url of denylistsUrls) {
-      await loadDenyListFromUrl(logger, components, url, deniedContentIdentifiers)
+  const loadDenylistFromFile = async () => {
+    if (!fileName) return
+    if (!(await components.fs.existPath(fileName))) {
+      logger.info("Denylist file doesn't exist", { fileName })
+      return
     }
 
-    logger.info('Load successfully the denylist')
-  } catch (err) {
-    logger.error(err)
+    const content = components.fs.createReadStream(fileName, { encoding: 'utf-8' })
+
+    const lines = createInterface({ input: content, crlfDelay: Infinity })
+
+    await processLines(lines)
   }
-}
 
-export async function createDenylistComponent(
-  components: Pick<AppComponents, 'env' | 'logs' | 'fs' | 'fetcher'>
-): Promise<DenylistComponent & IBaseComponent> {
-  const logger = components.logs.getLogger('Denylist')
-  const bannedList = new Set()
-
-  await reloadDenyLists(logger, components, bannedList)
-
-  const timer = setInterval(
-    () => reloadDenyLists(logger, components, bannedList).catch(logger.error),
-    120_000 /* two minutes */
-  )
-
-  const isDenyListed = (hash: string): boolean => {
-    return bannedList.has(hash)
+  const loadDenylistFromUrl = async (downloadableLink: URL) => {
+    try {
+      const response = await components.fetcher.fetch(downloadableLink.toString())
+      const content = await response.text()
+      const lines = content ? content.split(/[\r\n\s]+/) : []
+      await processLines(lines)
+    } catch (err: any) {
+      logger.error(err)
+    }
   }
+
+  const loadDenylists = async () => {
+    try {
+      const originalSize = deniedContentIdentifiers.size
+      await loadDenylistFromFile()
+
+      for (const url of denylistsUrls) {
+        await loadDenylistFromUrl(url)
+      }
+      if (deniedContentIdentifiers.size > originalSize) {
+        logger.info('New elements where added')
+      }
+    } catch (err) {
+      logger.error(err)
+    }
+  }
+
+  await loadDenylists()
+  const reloadTimer = setInterval(() => loadDenylists().catch(logger.error), 120_000 /* two minutes */)
 
   return {
-    isDenyListed,
+    isDenylisted: (id: string): boolean => {
+      return deniedContentIdentifiers.has(id)
+    },
     async stop() {
-      clearInterval(timer)
+      clearInterval(reloadTimer)
     }
   }
 }
