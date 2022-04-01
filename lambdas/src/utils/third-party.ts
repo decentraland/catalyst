2caf925122c6884318a4d8a793d51a698e4e8b9f
guidota <guido@decentraland.org>
2022-04-01 13:16:47 -0300

    refactor: allow both collection id and third party id as query param

--- lambdas/src/utils/third-party.ts
+++ lambdas/src/utils/third-party.ts
@@ -12,17 +12,17 @@ export interface ThirdPartyFetcher {
 }
 
 export const createThirdPartyFetcher = (): ThirdPartyFetcher => ({
-  fetchAssets: async (url: string, collectionId: string, owner: EthAddress): Promise<ThirdPartyAsset[]> => {
+  fetchAssets: async (url: string, registryId: string, owner: EthAddress): Promise<ThirdPartyAsset[]> => {
     try {
-      const assetsByOnwer = (await fetchJson(`${url}/registry/${collectionId}/address/${owner}/assets`, {
+      const assetsByOnwer = (await fetchJson(`${url}/registry/${registryId}/address/${owner}/assets`, {
         timeout: '5000'
       })) as ThirdPartyAssets
 
       if (!assetsByOnwer)
-        LOGGER.debug(`No assets found with owner: ${owner}, url: ${url} and registryId: ${collectionId}`)
+        LOGGER.debug(`No assets found with owner: ${owner}, url: ${url} and registryId: ${registryId}`)
       return assetsByOnwer?.assets ?? []
     } catch (e) {
-      throw new Error(`Error fetching assets with owner: ${owner}, url: ${url} and registryId: ${collectionId}`)
+      throw new Error(`Error fetching assets with owner: ${owner}, url: ${url} and registryId: ${registryId}`)
     }
   }
 })
@@ -32,14 +32,42 @@ export const createThirdPartyResolver = async (
   thirdPartyFetcher: ThirdPartyFetcher,
   collectionId: string
 ): Promise<FindWearablesByOwner> => {
-  const thirdPartyResolverAPI = await theGraphClient.findThirdPartyResolver('thirdPartyRegistrySubgraph', collectionId)
+  const thirdPartyId = parseCollectionId(collectionId)
+  const thirdPartyResolverAPI = await theGraphClient.findThirdPartyResolver(
+    'thirdPartyRegistrySubgraph',
+    thirdPartyId.thirdPartyId
+  )
   if (!thirdPartyResolverAPI) throw new Error(`Could not find third party resolver for collectionId: ${collectionId}`)
 
   return {
     findWearablesByOwner: async (owner) => {
-      const assetsByOwner = await thirdPartyFetcher.fetchAssets(thirdPartyResolverAPI, collectionId, owner)
+      const assetsByOwner = await thirdPartyFetcher.fetchAssets(thirdPartyResolverAPI, thirdPartyId.registryId, owner)
       if (!assetsByOwner) throw new Error(`Could not fetch assets for owner: ${owner}`)
       return assetsByOwner?.map((asset) => asset.urn.decentraland) ?? []
     }
+  }
+}
+
+// urn:decentraland:{protocol}:collections-thirdparty:{third-party-name}
+// urn:decentraland:{protocol}:collections-thirdparty:{third-party-name}:{collection-id}
+type ThirdPartyId = {
+  urn: string
+  thirdPartyId: string
+  registryId: string
+}
+
+const parseCollectionId = (collectionId: string): ThirdPartyId => {
+  const parts = collectionId.split(':')
+
+  if (!(parts.length === 5 || parts.length === 6)) {
+    throw new Error(`Couldn't parse collectionId ${collectionId}, valid ones are like:
+    \n - urn:decentraland:{protocol}:collections-thirdparty:{third-party-name}
+    \n - urn:decentraland:{protocol}:collections-thirdparty:{third-party-name}:{collection-id}`)
+  }
+
+  return {
+    urn: collectionId,
+    thirdPartyId: parts.slice(0, 5).join(':'),
+    registryId: parts[4]
   }
 }
