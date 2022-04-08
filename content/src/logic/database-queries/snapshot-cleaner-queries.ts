f8deed97630ea5ed439fc45939decaf201ba37bc
Pedro Tamborindeguy <pftambo@gmail.com>
2022-04-08 16:06:57 -0300

    fix: fix getting used hashes when hashes is empty

--- content/src/logic/database-queries/snapshot-cleaner-queries.ts
+++ content/src/logic/database-queries/snapshot-cleaner-queries.ts
@@ -5,10 +5,13 @@ export async function filterContentFileHashes(
   components: Pick<AppComponents, 'database'>,
   hashes: string[]
 ): Promise<string[]> {
-  const hashesList = hashes.map((p, i) => (i < hashes.length - 1 ? SQL`${p},` : SQL`${p}`))
-  const query = SQL`SELECT distinct(content_hash) FROM content_files WHERE content_hash IN (`
-  hashesList.forEach((v) => query.append(v))
-  query.append(`);`)
+  if (hashes.length === 0) {
+    return []
+  }
+
+  const joinedHashes = hashes.join(',')
+  const query = SQL`SELECT distinct(content_hash) FROM content_files WHERE content_hash IN (${joinedHashes});`
+
   const queryResultRows = (await components.database.queryWithValues<{ content_hash: string }>(query, 'used_hashes'))
     .rows
   return queryResultRows.map((row) => row.content_hash)
