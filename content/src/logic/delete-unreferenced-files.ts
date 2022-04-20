3f9f53b397ceb55f467e86bba3f340229aa061df
Pedro Tamborindeguy <pftambo@gmail.com>
2022-04-20 14:00:25 -0300

    chore: refactor with performance tracer

--- content/src/logic/delete-unreferenced-files.ts
+++ content/src/logic/delete-unreferenced-files.ts
@@ -1,3 +1,4 @@
+import { ILoggerComponent } from '@well-known-components/interfaces'
 import * as bf from 'bloom-filters'
 import PQueue from 'p-queue'
 import { AppComponents } from '../types'
@@ -7,85 +8,83 @@ import {
 } from './database-queries/unreferenced-files-queries'
 const { BloomFilter } = bf
 
-function printMemory(memoryMessage: string) {
-  console.log(memoryMessage)
-  const memoryUsage = process.memoryUsage()
-  const initBytesBefore = {
-    total: `${Math.round(memoryUsage.heapTotal / 1_000_000)}MB`,
-    used: `${Math.round(memoryUsage.heapUsed / 1_000_000)}MB`
+function createPerformanceTracer(logger: ILoggerComponent.ILogger) {
+  const timers: Map<string, number> = new Map()
+  const currentMemoryUsage = () => {
+    const memoryUsage = process.memoryUsage()
+    return JSON.stringify({
+      total: `${Math.round(memoryUsage.heapTotal / 1_000_000)}MB`,
+      used: `${Math.round(memoryUsage.heapUsed / 1_000_000)}MB`
+    })
   }
-  console.log(initBytesBefore)
+  return {
+    start(id: string) {
+      if (!timers.has(id)) {
+        logger.debug(`Starting ${id}. Memory: ${currentMemoryUsage()}`)
+        timers.set(id, performance.now())
+      } else {
+        logger.warn(`Starting timer '${id}' that is already started`)
+      }
+    },
+    end(id: string) {
+      const endTime = performance.now()
+      const startTime = timers.get(id)
+      if (startTime) {
+        const elapsedTime = endTime - startTime
+        timers.delete(id)
+        logger.debug(`Finished ${id}: ${elapsedTime.toFixed(3)}ms. Memory: ${currentMemoryUsage()}`)
+        timers.delete(id)
+      } else {
+        logger.warn(`Ending timer '${id}' that was not started`)
+      }
+    }
+  }
 }
 
 export async function deleteUnreferencedFiles(
   components: Pick<AppComponents, 'logs' | 'database' | 'storage'>
 ): Promise<void> {
   const logger = components.logs.getLogger('UnreferencedFilesDeleter')
-  // printMemory(`Memory usage before loading hashes:`)
-  // const [contentFileHashes, entityFileHashes, storageFileIds] = await Promise.all([
-  //   getContentFileHashes(components),
-  //   getEntityFileHashes(components),
-  //   components.storage.allFileIds()
-  // ])
-  // printMemory(`Memory usage after loading hashes:`)
-  // const fileHashes = new Set(contentFileHashes)
-  // entityFileHashes.forEach((hash) => fileHashes.add(hash))
-  // printMemory(`Memory usage after loading hashes in Set:`)
-  // const bloom = createBloomFilterComponent({ sizeInBytes: 10_485_760 })
-  // console.log(`#Content files: ${contentFileHashes.length}`)
-  // console.log(`#Entity files: ${entityFileHashes.length}`)
-  printMemory(`Memory usage before streaming entity ids`)
-  console.time('Creating bloom filter')
-  const newBloom = BloomFilter.create(15_000_000, 0.001)
-  console.time('Stream entity ids + add to bloomFilter')
-  let totalEntityIds = 0
-  for await (const entityId of streamAllDistinctEntityIds(components)) {
-    totalEntityIds++
-    newBloom.add(entityId)
+  const tracer = createPerformanceTracer(logger)
+  const referencedHashesBloom = BloomFilter.create(15_000_000, 0.001)
+
+  const addEntityIdsToBloomFilter = async () => {
+    tracer.start('to stream entiy ids + add to bloom filter')
+    let totalEntityIds = 0
+    for await (const entityId of streamAllDistinctEntityIds(components)) {
+      totalEntityIds++
+      referencedHashesBloom.add(entityId)
+    }
+    tracer.end('to stream entiy ids + add to bloom filter')
+    return totalEntityIds
   }
-  printMemory(`Memory usage after streaming entity ids`)
-  console.timeEnd('Stream entity ids + add to bloomFilter')
-  console.time('Stream content file hashes + add to bloomFilter')
-  let totalContentFileHashes = 0
-  for await (const contentFileHash of streamAllDistinctContentFileHashes(components)) {
-    totalContentFileHashes++
-    newBloom.add(contentFileHash)
+
+  const addContentFileHashesToBloomFilter = async () => {
+    tracer.start('to stream content file hashes + add to bloom filter')
+    let totalContentFileHashes = 0
+    for await (const contentFileHash of streamAllDistinctContentFileHashes(components)) {
+      totalContentFileHashes++
+      referencedHashesBloom.add(contentFileHash)
+    }
+    tracer.end('to stream content file hashes + add to bloom filter')
+    return totalContentFileHashes
   }
-  console.timeEnd('Stream content file hashes + add to bloomFilter')
-  printMemory(`Memory usage after streaming content file hashes`)
-  // contentFileHashes.forEach((hash) => newBloom.add(hash))
-  // entityFileHashes.forEach((hash) => newBloom.add(hash))
-  console.timeEnd('Creating bloom filter')
-  console.log(`Created bloom filter with: ${totalEntityIds} entity ids and ${totalContentFileHashes} content hashes.`)
+
+  const addReferencedHashesToBloomFilter = async () => {
+    tracer.start('to populate bloom filter')
+    const totalEntityIds = await addEntityIdsToBloomFilter()
+    const totalContentFileHashes = await addContentFileHashesToBloomFilter()
+    tracer.end('to populate bloom filter')
+    logger.info(`Created bloom filter with ${totalEntityIds} entity ids and ${totalContentFileHashes} content hashes.`)
+  }
+
+  await addReferencedHashesToBloomFilter()
 
   const queue = new PQueue({ concurrency: 1000 })
   let numberOfDeletedFiles = 0
-  // let numberOfDeletedFilesByBloom = 0
-  // let numberOfDeletedFilesBySet = 0
-  const storageFileIds = components.storage.allFileIds()
-  for await (const storageFileId of storageFileIds) {
-    // const notInBloom = !newBloom.has(storageFileId)
-    // const notInSet = !fileHashes.has(storageFileId)
-    // let debugMessage: string
-    // if (notInBloom && notInSet) {
-    //   debugMessage = `Deleting by both: ${storageFileId}`
-    //   numberOfDeletedFilesByBloom++
-    //   numberOfDeletedFilesBySet++
-    // } else if (notInBloom) {
-    //   debugMessage = `Deleting by bloom: ${storageFileId}`
-    //   numberOfDeletedFilesByBloom++
-    // } else if (notInSet) {
-    //   debugMessage = `Deleting by set: ${storageFileId}`
-    //   numberOfDeletedFilesBySet++
-    // }
-    // if (!newBloom.has(storageFileId)) {
-    // if (!newBloom.has(storageFileId) || !fileHashes.has(storageFileId)) {
-    // await queue.add(async () => {
-    // logger.debug(debugMessage)
-    // await components.storage.delete([storageFileId])
-    // })
-    // numberOfDeletedFiles++
-    if (!newBloom.has(storageFileId)) {
+
+  for await (const storageFileId of components.storage.allFileIds()) {
+    if (!referencedHashesBloom.has(storageFileId)) {
       await queue.add(async () => {
         logger.debug(`Deleting file by bloom filter: ${storageFileId}`)
         // await components.storage.delete([storageFileId])
@@ -95,10 +94,4 @@ export async function deleteUnreferencedFiles(
   }
   // To Do: Log size released?
   logger.info(`Deleted ${numberOfDeletedFiles} files`)
-  // logger.info(`Deleted ${numberOfDeletedFilesByBloom} files by bloom`)
-  // logger.info(`Deleted ${numberOfDeletedFilesBySet} files by set`)
-  // logger.info(`False positives in bloom filter: ${numberOfDeletedFilesBySet - numberOfDeletedFilesByBloom}`)
-  // logger.info(
-  // `False positives rate: ${(numberOfDeletedFilesBySet - numberOfDeletedFilesByBloom) / numberOfDeletedFilesBySet}`
-  // )
 }
