5d17f45160102d5d0a1acea6f758a5d103e6effa
Pedro Tamborindeguy <pftambo@gmail.com>
2022-04-29 15:38:59 -0300

    test: test CI without testcontainers

--- content/jest.globalSetup.ts
+++ content/jest.globalSetup.ts
@@ -1,48 +1,40 @@
 import { CONTENT_API } from '@dcl/catalyst-api-specs'
-import { exec } from 'child_process'
-import Dockerode from "dockerode"
 import fs from 'fs'
 import path from 'path'
-import { PassThrough } from 'stream'
-import { GenericContainer } from 'testcontainers'
-import { LogWaitStrategy } from 'testcontainers/dist/wait-strategy'
-import { promisify } from 'util'
-import { DEFAULT_DATABASE_CONFIG } from './src/Environment'
-import { E2ETestEnvironment } from './test/integration/E2ETestEnvironment'
 import { isCI } from './test/integration/E2ETestUtils'
 
-const execute = promisify(exec)
-const postgresContainerName = 'postgres_test'
+// const execute = promisify(exec)
+// const postgresContainerName = 'postgres_test'
 
-const deletePreviousPsql = async (): Promise<void> => {
-  const { stderr, stdout } = await execute(`docker rm -f ${postgresContainerName}`)
-  if (stderr && !stderr.includes(`Error: No such container: ${postgresContainerName}`)) {
-    console.log('Failed to delete the existing postgres container')
-  } else if (stdout) {
-    console.log('Deleted the previous container')
-  }
-}
+// const deletePreviousPsql = async (): Promise<void> => {
+//   const { stderr, stdout } = await execute(`docker rm -f ${postgresContainerName}`)
+//   if (stderr && !stderr.includes(`Error: No such container: ${postgresContainerName}`)) {
+//     console.log('Failed to delete the existing postgres container')
+//   } else if (stdout) {
+//     console.log('Deleted the previous container')
+//   }
+// }
 
 const globalSetup = async (): Promise<void> => {
   if (!isCI()) {
     // delete postgres_test container if it exists
-    await deletePreviousPsql()
+    // await deletePreviousPsql()
 
-    // start postgres container and wait for it to be ready
-    const container = await new GenericContainer('postgres:12')
-      .withName(postgresContainerName)
-      .withEnv('POSTGRES_PASSWORD', DEFAULT_DATABASE_CONFIG.password)
-      .withEnv('POSTGRES_USER', DEFAULT_DATABASE_CONFIG.user)
-      .withExposedPorts(E2ETestEnvironment.POSTGRES_PORT)
-      .withWaitStrategy(new PostgresWaitStrategy())
-      // .withWaitStrategy(Wait.forLogMessage('database system is ready to accept connections'))
-      .start()
+    // // start postgres container and wait for it to be ready
+    // const container = await new GenericContainer('postgres:12')
+    //   .withName(postgresContainerName)
+    //   .withEnv('POSTGRES_PASSWORD', DEFAULT_DATABASE_CONFIG.password)
+    //   .withEnv('POSTGRES_USER', DEFAULT_DATABASE_CONFIG.user)
+    //   .withExposedPorts(E2ETestEnvironment.POSTGRES_PORT)
+    //   .withWaitStrategy(new PostgresWaitStrategy())
+    //   .start()
 
-    globalThis.__POSTGRES_CONTAINER__ = container
-    // get mapped port to be used for testing purposes
-    process.env.MAPPED_POSTGRES_PORT = container.getMappedPort(E2ETestEnvironment.POSTGRES_PORT).toString()
+    // globalThis.__POSTGRES_CONTAINER__ = container
+    // // get mapped port to be used for testing purposes
+    // process.env.MAPPED_POSTGRES_PORT = container.getMappedPort(E2ETestEnvironment.POSTGRES_PORT).toString()
+    console.log('im not CI!')
   }
-
+  console.log('im in CI')
   // Initialize API Coverage Report
   if (process.env.API_COVERAGE === 'true' || isCI()) {
     await initializeApiCoverage()
@@ -50,43 +42,43 @@ const globalSetup = async (): Promise<void> => {
 }
 
 /** During startup, the db is restarted, so we need to wait for the log message twice */
-class PostgresWaitStrategy extends LogWaitStrategy {
-  private static LOG = 'database system is ready to accept connections'
-  private DOCKERODE = new Dockerode()
-  constructor() {
-    super(PostgresWaitStrategy.LOG)
-  }
+// class PostgresWaitStrategy extends LogWaitStrategy {
+//   private static LOG = 'database system is ready to accept connections'
+//   private DOCKERODE = new Dockerode()
+//   constructor() {
+//     super(PostgresWaitStrategy.LOG)
+//   }
 
-  public async waitUntilReady(container: Dockerode.Container): Promise<void> {
-    let counter = 0
-    return new Promise(async (resolve, reject) => {
-      const stream = await container.logs({ stdout: true, stderr: true, follow: true })
-      const demuxedStream = new PassThrough({ autoDestroy: true, encoding: "utf-8" });
-      this.DOCKERODE.modem.demuxStream(stream, demuxedStream, demuxedStream);
-      stream.on("end", () => demuxedStream.end());
-      demuxedStream
-        .on('data', (line) => {
-          if (line.toString().includes(PostgresWaitStrategy.LOG)) {
-            counter++
-            if (counter === 2) {
-              resolve()
-            }
-          }
-        })
-        .on('err', (line) => {
-          if (line.toString().includes(PostgresWaitStrategy.LOG)) {
-            counter++
-            if (counter === 2) {
-              resolve()
-            }
-          }
-        })
-        .on('end', () => {
-          reject()
-        })
-    })
-  }
-}
+//   public async waitUntilReady(container: Dockerode.Container): Promise<void> {
+//     let counter = 0
+//     return new Promise(async (resolve, reject) => {
+//       const stream = await container.logs({ stdout: true, stderr: true, follow: true })
+//       const demuxedStream = new PassThrough({ autoDestroy: true, encoding: "utf-8" });
+//       this.DOCKERODE.modem.demuxStream(stream, demuxedStream, demuxedStream);
+//       stream.on("end", () => demuxedStream.end());
+//       demuxedStream
+//         .on('data', (line) => {
+//           if (line.toString().includes(PostgresWaitStrategy.LOG)) {
+//             counter++
+//             if (counter === 2) {
+//               resolve()
+//             }
+//           }
+//         })
+//         .on('err', (line) => {
+//           if (line.toString().includes(PostgresWaitStrategy.LOG)) {
+//             counter++
+//             if (counter === 2) {
+//               resolve()
+//             }
+//           }
+//         })
+//         .on('end', () => {
+//           reject()
+//         })
+//     })
+//   }
+// }
 
 export type ApiCoverage = {
   [path: string]: {
