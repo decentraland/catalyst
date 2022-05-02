03924a62b1e276f735e3e5c9d0dade7f5e441f83
Pedro Tamborindeguy <pftambo@gmail.com>
2022-05-02 12:55:55 -0300

    chore: add comment for testcontainers dynamically imported

--- content/jest.globalSetup.ts
+++ content/jest.globalSetup.ts
@@ -6,6 +6,11 @@ import { isCI } from './test/integration/E2ETestUtils'
 
 const globalSetup = async (): Promise<void> => {
   if (!isCI()) {
+    /**
+     * The simple fact of importing 'testcontainers' included in './test/postgres-test-container'
+     * triggers thet lib to connect to the Docker client, which fails.
+     * So, we need to dynamically import it only if we're not running the tests in CI.
+     */
     tsNode.register({ transpileOnly: true })
     const { initializePostgresContainer } = await import('./test/postgres-test-container')
     await initializePostgresContainer('postgres_test')
