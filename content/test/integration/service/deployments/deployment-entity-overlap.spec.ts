053ff595331aff2438b2cae683e91bd522b3abf4
Mariano Goldman <mariano.goldman@decentraland.org>
2022-05-16 17:05:56 -0300

    test: more tests

--- content/test/integration/service/deployments/deployment-entity-overlap.spec.ts
+++ content/test/integration/service/deployments/deployment-entity-overlap.spec.ts
@@ -6,22 +6,23 @@ import {
   isSuccessfulDeployment
 } from "../../../../src/service/Service";
 import { AppComponents } from "../../../../src/types";
-import { makeNoopServerValidator } from "../../../helpers/service/validations/NoOpValidator";
+import { makeNoopServerValidator, makeNoopValidator } from "../../../helpers/service/validations/NoOpValidator";
 import { loadStandaloneTestEnvironment, testCaseWithComponents } from "../../E2ETestEnvironment";
-import { buildDeployData, buildDeployDataAfterEntity, EntityCombo } from "../../E2ETestUtils";
+import { buildDeployData, buildDeployDataAfterEntity, createIdentity, EntityCombo, Identity } from "../../E2ETestUtils";
 import { stub } from "sinon";
 
 /**
- * This test verifies that after a parcel changed owner, a new deployment from new owner is allowed
- * and previous overlapping scenes end up with no scene.
+ * This test verifies some scenarios that could happen during scene deployments.
  */
 loadStandaloneTestEnvironment()("Integration - Deployment with Entity Overlaps", (testEnv) => {
   const P1 = "0,0";
   const P2 = "0,1";
   const P3 = "1,1";
+  let identity: Identity;
   let E1: EntityCombo, E2: EntityCombo;
 
   beforeEach(async () => {
+    identity = createIdentity();
     E1 = await buildDeployData([P1, P2], {
       type: EntityType.SCENE,
       metadata: {
@@ -30,24 +31,92 @@ loadStandaloneTestEnvironment()("Integration - Deployment with Entity Overlaps",
           base: P1,
           parcels: [P1, P2]
         }
-      }
+      },
+      identity
     });
   });
 
   testCaseWithComponents(
     testEnv,
-    "When parcel changes owner, then new deployment by new owner succeeds and removes previous scenes from other parcels",
+    "When new scene is deployed on overlapping parcels, then new deployment removes previous scenes from orphaned parcels",
     async (components) => {
-      // make noop server validator
+      // make validators stub
+      makeNoopValidator(components);
       makeNoopServerValidator(components);
 
-      stub(components.externalCalls, 'queryGraph')
-        .callsFake((...args: any) => {
-          console.log("queryGraph called with", args);
-          return Promise.resolve({});
-        });
+      E2 = await buildDeployDataAfterEntity(E1, [P2], {
+        type: EntityType.SCENE,
+        metadata: {
+          main: "main.js",
+          scene: {
+            base: P2,
+            parcels: [P2]
+          }
+        },
+        identity
+      });
 
-      // make stub validator
+      // Deploy E1 on P1, P2
+      await deploy(components, E1);
+      await assertDeploymentsAre(components, E1);
+
+      // Deploy E2 on P2
+      await deploy(components, E2);
+      await assertDeploymentsAre(components, E2); // E1 should no longer be active
+    }
+  );
+
+  testCaseWithComponents(
+    testEnv,
+    "When scene is deployed, then server checks for permissions",
+    async (components) => {
+      // make validators stub
+      stub(components.validator, "validate")
+        .onFirstCall()
+        .resolves({ ok: true })
+        .onSecondCall()
+        .resolves({
+          ok: false, errors: [
+            `The provided Eth Address does not have access to the following parcel: (${P1})`,
+            `The provided Eth Address does not have access to the following parcel: (${P2})`
+          ]
+        });
+      makeNoopServerValidator(components);
+
+      E2 = await buildDeployDataAfterEntity(E1, [P1, P2], {
+        type: EntityType.SCENE,
+        metadata: {
+          main: "main.js",
+          scene: {
+            base: P1,
+            parcels: [P1, P2]
+          }
+        }
+      });
+
+      // Deploy E1 on P1, P2
+      await deploy(components, E1);
+      await assertDeploymentsAre(components, E1);
+
+      // Deploy E2 on P1
+      await expect(deploy(components, E2)).rejects.toThrow('The provided Eth Address does not have access to the following parcel: (0,0),The provided Eth Address does not have access to the following parcel: (0,1)');
+      await assertDeploymentsAre(components, E1); // E2 should have not been deployed
+    }
+  );
+
+  testCaseWithComponents(
+    testEnv,
+    "When parcel changes owner, then new deployment by new owner succeeds and removes previous scenes from orphaned parcels",
+    async (components) => {
+      // make noop server validator
+      makeNoopValidator(components);
+      makeNoopServerValidator(components);
+
+      // stub(components.externalCalls, 'queryGraph')
+      //   .callsFake((...args: any) => {
+      //     console.log("queryGraph called with", args);
+      //     return Promise.resolve({});
+      //   });
 
       // Deploy E1 on P1, P2
       await deploy(components, E1);
@@ -78,7 +147,6 @@ loadStandaloneTestEnvironment()("Integration - Deployment with Entity Overlaps",
     ...expectedEntities: EntityCombo[]
   ) {
     const actualDeployments = await components.deployer.getDeployments();
-    console.log(actualDeployments);
     const expectedEntityIds = expectedEntities.map((entityCombo) => entityCombo.entity.id).sort();
     const actualEntityIds = actualDeployments.deployments.map(({ entityId }) => entityId).sort();
     expect({ deployedEntityIds: actualEntityIds }).toEqual({ deployedEntityIds: expectedEntityIds });
