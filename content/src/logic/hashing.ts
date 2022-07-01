fc0a85066d3a6d9523de536f48ee04fa0bde6f25
Agustina Aldasoro <agusaldasoro@users.noreply.github.com>
2022-07-01 13:25:50 -0300

    fix: revert changes to support syncing old entities (#1158)
    * fix: revert changes to support syncing old entities
    
    * Add test for deploying an old entity

--- content/src/logic/hashing.ts
+++ content/src/logic/hashing.ts
@@ -1,8 +1,18 @@
-import { hashV1 } from '@dcl/hashing'
+import { hashV0, hashV1 } from '@dcl/hashing'
 
 export async function calculateIPFSHashes<T extends Uint8Array>(files: T[]): Promise<{ hash: string; file: T }[]> {
   const entries = Array.from(files).map(async (file) => ({
     hash: await hashV1(file),
+    file
+  }))
+  return Promise.all(entries)
+}
+
+export async function calculateDeprecatedHashes<T extends Uint8Array>(
+  files: T[]
+): Promise<{ hash: string; file: T }[]> {
+  const entries = Array.from(files).map(async (file) => ({
+    hash: await hashV0(file),
     file
   }))
   return Promise.all(entries)
