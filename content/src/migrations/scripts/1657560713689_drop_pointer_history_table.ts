f16c93dab154de92693dd7277646b49fa579a5bb
Mariano Goldman <marianogoldman@users.noreply.github.com>
2022-07-11 17:01:52 -0300

    refactor: remove pointer_history and last_deployed_pointers tables (#1172)

---
+++ content/src/migrations/scripts/1657560713689_drop_pointer_history_table.ts
@@ -0,0 +1,21 @@
+import { MigrationBuilder } from 'node-pg-migrate'
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable('pointer_history')
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable(
+    'pointer_history',
+    {
+      pointer: { type: 'text', notNull: true },
+      entity_type: { type: 'text', notNull: true },
+      deployment: { type: 'integer', references: 'deployments', notNull: true }
+    },
+    {
+      constraints: {
+        unique: ['pointer', 'entity_type', 'deployment']
+      }
+    }
+  )
+}
---
+++ content/src/migrations/scripts/1657560812218_drop_last_deployed_pointers_table.ts
@@ -0,0 +1,21 @@
+import { MigrationBuilder } from 'node-pg-migrate'
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable('last_deployed_pointers')
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable(
+    'last_deployed_pointers',
+    {
+      pointer: { type: 'text', notNull: true },
+      entity_type: { type: 'text', notNull: true },
+      deployment: { type: 'integer', references: 'deployments', notNull: true }
+    },
+    {
+      constraints: {
+        unique: ['pointer', 'entity_type']
+      }
+    }
+  )
+}
