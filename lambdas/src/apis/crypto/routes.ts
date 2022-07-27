5f24fb42c90348721eb68a6948bd79a9f371f89f
Pedro Tamborindeguy <pftambo@gmail.com>
2022-07-27 17:16:04 -0300

    refactor: move images apis

---
+++ lambdas/src/apis/crypto/routes.ts
@@ -0,0 +1,15 @@
+import { HTTPProvider } from 'eth-connect'
+import { Request, Response, Router } from 'express'
+import { validateSignature } from './controllers/crypto'
+
+export function initializeCryptoRoutes(router: Router, ethereumProvider: HTTPProvider): Router {
+  router.post('/validate-signature', createHandler(ethereumProvider, validateSignature))
+  return router
+}
+
+function createHandler(
+  ethereumProvider: HTTPProvider,
+  originalHandler: (ethereumProvider: HTTPProvider, req: Request, res: Response) => void
+): (req: Request, res: Response) => void {
+  return (req: Request, res: Response) => originalHandler(ethereumProvider, req, res)
+}
