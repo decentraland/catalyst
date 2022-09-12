d7e4cd3b59cd9c37781a0318849cbdd5d4bd74ec
Mariano Goldman <marianogoldman@users.noreply.github.com>
2022-09-12 14:34:50 -0300

    refactor: use the graph wkc in lambdas (#1237)

--- lambdas/src/metrics.ts
+++ lambdas/src/metrics.ts
@@ -1,8 +1,10 @@
 import { createTestMetricsComponent, validateMetricsDeclaration } from '@well-known-components/metrics'
 import { getDefaultHttpMetrics } from '@well-known-components/metrics/dist/http'
+import { metricDeclarations as theGraphMetrics } from '@well-known-components/thegraph-component'
 
 export const metrics = validateMetricsDeclaration({
-  ...getDefaultHttpMetrics()
+  ...getDefaultHttpMetrics(),
+  ...theGraphMetrics
 })
 
 export const metricsComponent = createTestMetricsComponent(metrics)
