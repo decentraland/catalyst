3bd13c05e69009e6bf1874fc1d7e030d34783891
Mariano Goldman <mariano.goldman@decentraland.org>
2022-10-31 09:29:57 -0300

    fix metrics instrumentation

--- lambdas/src/metrics.ts
+++ lambdas/src/metrics.ts
@@ -1,5 +1,8 @@
-import { createTestMetricsComponent, validateMetricsDeclaration } from '@well-known-components/metrics'
-import { getDefaultHttpMetrics } from '@well-known-components/metrics/dist/http'
+import {
+  createTestMetricsComponent,
+  getDefaultHttpMetrics,
+  validateMetricsDeclaration
+} from '@well-known-components/metrics'
 import { metricDeclarations as theGraphMetrics } from '@well-known-components/thegraph-component'
 
 export const metrics = validateMetricsDeclaration({
