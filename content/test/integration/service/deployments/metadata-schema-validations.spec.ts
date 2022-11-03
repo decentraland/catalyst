55d71c77b4520ca59ad5f9862b7347bb33f37185
Mariano Goldman <mariano.goldman@decentraland.org>
2022-11-03 11:45:58 -0300

    fix test

--- content/test/integration/service/deployments/metadata-schema-validations.spec.ts
+++ content/test/integration/service/deployments/metadata-schema-validations.spec.ts
@@ -104,13 +104,11 @@ loadStandaloneTestEnvironment()("Integration - Deployment with metadata validati
         identity,
       })
 
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The provided Eth Address does not have access to the following parcel: (0,0)",
-            "The provided Eth Address does not have access to the following parcel: (0,1)"
-          ]
-        })
+      const deploymentResult: DeploymentResult = await deployEntity(components, E1);
+      expect(deploymentResult['errors'])
+        .toContain("The provided Eth Address does not have access to the following parcel: (0,0)")
+      expect(deploymentResult['errors'])
+        .toContain("The provided Eth Address does not have access to the following parcel: (0,1)")
     }
   )
 
