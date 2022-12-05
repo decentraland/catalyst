9ba79cda8913ad0a7d37b9b17baf33eeb724f3f8
Pedro Tamborindeguy <pftambo@gmail.com>
2022-12-05 15:48:01 -0300

    fix: build

--- content/src/service/synchronization/bootstrapFromSnapshots.ts
+++ content/src/service/synchronization/bootstrapFromSnapshots.ts
@@ -1,63 +1,61 @@
-import {
-  getDeployedEntitiesInSnapshotsStreamFromServers
-} from '@dcl/snapshots-fetcher'
-import { AppComponents } from '../../types'
+// import { getDeployedEntitiesInSnapshotsStreamFromServers } from '@dcl/snapshots-fetcher'
+// import { AppComponents } from '../../types'
 
-type BootstrapComponents = Pick<
-  AppComponents,
-  | 'staticConfigs'
-  | 'logs'
-  | 'batchDeployer'
-  | 'metrics'
-  | 'fetcher'
-  | 'downloadQueue'
-  | 'contentCluster'
-  | 'storage'
-  | 'processedSnapshotStorage'
-  | 'processedSnapshots'
->
+// type BootstrapComponents = Pick<
+//   AppComponents,
+//   | 'staticConfigs'
+//   | 'logs'
+//   | 'batchDeployer'
+//   | 'metrics'
+//   | 'fetcher'
+//   | 'downloadQueue'
+//   | 'contentCluster'
+//   | 'storage'
+//   | 'processedSnapshotStorage'
+//   | 'processedSnapshots'
+// >
 
-/**
- * This function fetches all the full snapshots from remote catalysts and
- * then iterates over all of the deployments to call the batch deployer for each deployed entity.
- */
-export async function bootstrapFromSnapshots(components: BootstrapComponents): Promise<void> {
-  components.metrics.observe('dcl_content_server_sync_state', {}, 0)
-  // then find all other DAO catalyst servers
-  const catalystServersButThisOne = await components.contentCluster.getContentServersFromDao()
+// /**
+//  * This function fetches all the full snapshots from remote catalysts and
+//  * then iterates over all of the deployments to call the batch deployer for each deployed entity.
+//  */
+// export async function bootstrapFromSnapshots(components: BootstrapComponents): Promise<void> {
+//   components.metrics.observe('dcl_content_server_sync_state', {}, 0)
+//   // then find all other DAO catalyst servers
+//   const catalystServersButThisOne = await components.contentCluster.getContentServersFromDao()
 
-  const logs = components.logs.getLogger('BootstrapFromSnapshots')
-  logs.info(`Starting to bootstrap from snapshots`)
-  const requestMaxRetries = 2
-  const requestRetryWaitTime = 1000
+//   const logs = components.logs.getLogger('BootstrapFromSnapshots')
+//   logs.info(`Starting to bootstrap from snapshots`)
+//   const requestMaxRetries = 2
+//   const requestRetryWaitTime = 1000
 
-  // wait to get all the bootstrap data from all servers
-  await Promise.all(
-    catalystServersButThisOne.map(async (contentServer) => {
-      logs.info(`Will deploy entities from ${contentServer} snapshots`)
-      try {
-        const stream = getDeployedEntitiesInSnapshotsStreamFromServers(
-          components,
-          {
-            tmpDownloadFolder: components.staticConfigs.tmpDownloadFolder,
-            pointerChangesWaitTime: 0, // zero to not restart the timer
-            requestMaxRetries,
-            requestRetryWaitTime,
-            fromTimestamp: 0 // start bootstrap from the beginning of the times every time. it is cheap
-          },
-          catalystServersButThisOne
-        )
-        for await (const entity of stream) {
-          // schedule the deployment in the deployer. the await DOES NOT mean that the entity was deployed entirely.
-          await components.batchDeployer.deployEntity(entity, [contentServer])
-        }
-      } catch (error: any) {
-        logs.warn(`There was an error deploying entities from ${contentServer} snapshots`, error)
-      }
-    })
-  )
+//   // wait to get all the bootstrap data from all servers
+//   await Promise.all(
+//     catalystServersButThisOne.map(async (contentServer) => {
+//       logs.info(`Will deploy entities from ${contentServer} snapshots`)
+//       try {
+//         const stream = getDeployedEntitiesInSnapshotsStreamFromServers(
+//           components,
+//           {
+//             tmpDownloadFolder: components.staticConfigs.tmpDownloadFolder,
+//             pointerChangesWaitTime: 0, // zero to not restart the timer
+//             requestMaxRetries,
+//             requestRetryWaitTime,
+//             fromTimestamp: 0 // start bootstrap from the beginning of the times every time. it is cheap
+//           },
+//           catalystServersButThisOne
+//         )
+//         for await (const entity of stream) {
+//           // schedule the deployment in the deployer. the await DOES NOT mean that the entity was deployed entirely.
+//           await components.batchDeployer.deployEntity(entity, [contentServer])
+//         }
+//       } catch (error: any) {
+//         logs.warn(`There was an error deploying entities from ${contentServer} snapshots`, error)
+//       }
+//     })
+//   )
 
-  // wait for background jobs to finish
-  await components.downloadQueue.onIdle()
-  await components.batchDeployer.onIdle()
-}
+//   // wait for background jobs to finish
+//   await components.downloadQueue.onIdle()
+//   await components.batchDeployer.onIdle()
+// }
