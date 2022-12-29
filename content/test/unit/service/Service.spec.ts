47f2326f7754f63bcf04f22df057d456e101e944
Pedro Tamborindeguy <pftambo@gmail.com>
2022-12-29 18:28:14 -0300

    fix: test

--- content/test/unit/service/Service.spec.ts
+++ content/test/unit/service/Service.spec.ts
@@ -67,7 +67,7 @@ describe('Service', function () {
   })
 
   it(`When no file matches the given entity id, then deployment fails`, async () => {
-    jest.spyOn(failedDeploymentQueries, 'getFailedDeployments').mockResolvedValue([])
+    jest.spyOn(failedDeploymentQueries, 'getSnapshotFailedDeployments').mockResolvedValue([])
     jest.spyOn(failedDeploymentQueries, 'deleteFailedDeployment').mockResolvedValue()
     const service = await buildService()
     const deploymentResult = await service.deployEntity(
@@ -84,7 +84,7 @@ describe('Service', function () {
   })
 
   it(`When an entity is successfully deployed, then the content is stored correctly`, async () => {
-    jest.spyOn(failedDeploymentQueries, 'getFailedDeployments').mockResolvedValue([])
+    jest.spyOn(failedDeploymentQueries, 'getSnapshotFailedDeployments').mockResolvedValue([])
     jest.spyOn(failedDeploymentQueries, 'deleteFailedDeployment').mockResolvedValue()
     const service = await buildService()
     const storageSpy = jest.spyOn(service.components.storage, 'storeStream')
