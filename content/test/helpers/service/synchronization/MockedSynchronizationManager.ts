ff0f167c42ec36189c4ce689867f35fb40812899
Pedro Tamborindeguy <pftambo@gmail.com>
2023-01-04 13:46:26 -0300

    chore: remove unused files

--- content/test/helpers/service/synchronization/MockedSynchronizationManager.ts
+++
@@ -1,5 +0,0 @@
-// import { ISynchronizationManager } from '../../../../src/service/synchronization/SynchronizationManager'
-
-// export function makeNoopSynchronizationManager(component: ISynchronizationManager) {
-//   jest.spyOn(component, 'syncWithServers').mockResolvedValue()
-// }
