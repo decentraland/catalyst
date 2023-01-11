d972bc1646894cb9b677b4ad2b3f40ded2683c55
Hugo Arregui <hugo@decentraland.org>
2023-01-11 12:17:08 -0300

    Save Wed Jan 11 12:17:08 PM -03 2023

--- content/test/integration/service/deployments/metadata-schema-validations.spec.ts
+++ content/test/integration/service/deployments/metadata-schema-validations.spec.ts
@@ -1,122 +1,115 @@
-import { EntityType } from "@dcl/schemas"
-import { AuditInfo } from "../../../../src/service/deployments/types"
-import { DeploymentContext, DeploymentResult } from "../../../../src/service/Service"
+import { EntityType } from '@dcl/schemas'
+import { AuditInfo } from '../../../../src/service/deployments/types'
+import { DeploymentContext, DeploymentResult } from '../../../../src/service/Service'
 import { AppComponents, EntityVersion } from '../../../../src/types'
-import { makeNoopServerValidator } from "../../../helpers/service/validations/NoOpValidator"
-import { loadStandaloneTestEnvironment, testCaseWithComponents } from "../../E2ETestEnvironment"
-import { buildDeployData, createIdentity, EntityCombo } from "../../E2ETestUtils"
+import { makeNoopServerValidator } from '../../../helpers/service/validations/NoOpValidator'
+import { setupTestEnvironment, testCaseWithComponents } from '../../E2ETestEnvironment'
+import { buildDeployData, createIdentity, EntityCombo } from '../../E2ETestUtils'
 
-loadStandaloneTestEnvironment()("Integration - Deployment with metadata validation", (testEnv) => {
+describe('Integration - Deployment with metadata validation', () => {
+  const getTestEnv = setupTestEnvironment()
 
   testCaseWithComponents(
-    testEnv,
-    "When scene metadata is missing, deployment result should include the proper error",
+    getTestEnv,
+    'When scene metadata is missing, deployment result should include the proper error',
     async (components) => {
       makeNoopServerValidator(components)
 
-      const P1 = "0,0"
-      const P2 = "0,1"
+      const P1 = '0,0'
+      const P2 = '0,1'
       let E1: EntityCombo = await buildDeployData([P1, P2], {
         type: EntityType.SCENE,
         metadata: { a: 'metadata' }
       })
 
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The metadata for this entity type (scene) is not valid.",
-            "must have required property 'main'",
-            "must have required property 'scene'"
-          ]
-        })
+      expect(await deployEntity(components, E1)).toEqual({
+        errors: [
+          'The metadata for this entity type (scene) is not valid.',
+          "must have required property 'main'",
+          "must have required property 'scene'"
+        ]
+      })
     }
   )
 
   testCaseWithComponents(
-    testEnv,
-    "When scene metadata is present but incomplete, deployment result should include the proper errors",
+    getTestEnv,
+    'When scene metadata is present but incomplete, deployment result should include the proper errors',
     async (components) => {
       makeNoopServerValidator(components)
 
-      const P1 = "0,0"
-      const P2 = "0,1"
+      const P1 = '0,0'
+      const P2 = '0,1'
+      let E1: EntityCombo = await buildDeployData([P1, P2], {
+        type: EntityType.SCENE,
+        metadata: {}
+      })
+
+      expect(await deployEntity(components, E1)).toEqual({
+        errors: [
+          'The metadata for this entity type (scene) is not valid.',
+          "must have required property 'main'",
+          "must have required property 'scene'"
+        ]
+      })
+    }
+  )
+
+  testCaseWithComponents(
+    getTestEnv,
+    'When scene metadata is present but incomplete (missing scene), deployment result should include the proper error',
+    async (components) => {
+      makeNoopServerValidator(components)
+
+      const P1 = '0,0'
+      const P2 = '0,1'
       let E1: EntityCombo = await buildDeployData([P1, P2], {
         type: EntityType.SCENE,
         metadata: {
+          main: 'main.js'
         }
       })
 
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The metadata for this entity type (scene) is not valid.",
-            "must have required property 'main'",
-            "must have required property 'scene'"
-          ]
-        })
+      expect(await deployEntity(components, E1)).toEqual({
+        errors: ['The metadata for this entity type (scene) is not valid.', "must have required property 'scene'"]
+      })
     }
   )
 
   testCaseWithComponents(
-    testEnv,
-    "When scene metadata is present but incomplete (missing scene), deployment result should include the proper error",
+    getTestEnv,
+    'When scene metadata is present and ok, deployment fail because of permissions validator',
     async (components) => {
       makeNoopServerValidator(components)
 
-      const P1 = "0,0"
-      const P2 = "0,1"
-      let E1: EntityCombo = await buildDeployData([P1, P2], {
-        type: EntityType.SCENE,
-        metadata: {
-          main: "main.js"
-        }
-      })
-
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The metadata for this entity type (scene) is not valid.",
-            "must have required property 'scene'"
-          ]
-        })
-    }
-  )
-
-  testCaseWithComponents(
-    testEnv,
-    "When scene metadata is present and ok, deployment fail because of permissions validator",
-    async (components) => {
-      makeNoopServerValidator(components)
-
-      const P1 = "0,0"
-      const P2 = "0,1"
+      const P1 = '0,0'
+      const P2 = '0,1'
       const identity = createIdentity()
 
       let E1: EntityCombo = await buildDeployData([P1, P2], {
         type: EntityType.SCENE,
         metadata: {
-          main: "main.js",
+          main: 'main.js',
           scene: {
             base: P1,
             parcels: [P1, P2]
           }
         },
-        identity,
+        identity
       })
 
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The provided Eth Address does not have access to the following parcel: (0,0)",
-            "The provided Eth Address does not have access to the following parcel: (0,1)"
-          ]
-        })
+      expect(await deployEntity(components, E1)).toEqual({
+        errors: [
+          'The provided Eth Address does not have access to the following parcel: (0,0)',
+          'The provided Eth Address does not have access to the following parcel: (0,1)'
+        ]
+      })
     }
   )
 
   testCaseWithComponents(
-    testEnv,
-    "When profile metadata is missing, deployment result should include the proper error",
+    getTestEnv,
+    'When profile metadata is missing, deployment result should include the proper error',
     async (components) => {
       makeNoopServerValidator(components)
 
@@ -124,46 +117,37 @@ loadStandaloneTestEnvironment()("Integration - Deployment with metadata validati
       let E1: EntityCombo = await buildDeployData([identity.address], {
         type: EntityType.PROFILE,
         metadata: { a: 'metadata' },
-        identity,
+        identity
       })
 
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The metadata for this entity type (profile) is not valid.",
-            "must have required property 'avatars'"
-          ]
-        })
+      expect(await deployEntity(components, E1)).toEqual({
+        errors: ['The metadata for this entity type (profile) is not valid.', "must have required property 'avatars'"]
+      })
     }
   )
 
   testCaseWithComponents(
-    testEnv,
-    "When profile metadata is present but incomplete (missing avatars), deployment result should include the proper error",
+    getTestEnv,
+    'When profile metadata is present but incomplete (missing avatars), deployment result should include the proper error',
     async (components) => {
       makeNoopServerValidator(components)
 
       const identity = createIdentity()
       let E1: EntityCombo = await buildDeployData([identity.address], {
         type: EntityType.PROFILE,
-        metadata: {
-        },
-        identity,
+        metadata: {},
+        identity
       })
 
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The metadata for this entity type (profile) is not valid.",
-            "must have required property 'avatars'"
-          ]
-        })
+      expect(await deployEntity(components, E1)).toEqual({
+        errors: ['The metadata for this entity type (profile) is not valid.', "must have required property 'avatars'"]
+      })
     }
   )
 
   testCaseWithComponents(
-    testEnv,
-    "When wearable metadata is wrong, deployment result should include the proper error",
+    getTestEnv,
+    'When wearable metadata is wrong, deployment result should include the proper error',
     async (components) => {
       makeNoopServerValidator(components)
 
@@ -171,83 +155,80 @@ loadStandaloneTestEnvironment()("Integration - Deployment with metadata validati
       let E1: EntityCombo = await buildDeployData([identity.address], {
         type: EntityType.WEARABLE,
         metadata: { a: 'metadata' },
-        identity,
+        identity
       })
 
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The metadata for this entity type (wearable) is not valid.",
-            "must have required property 'collectionAddress'",
-            "must have required property 'rarity'",
-            "must pass \"_isThirdParty\" keyword validation",
-            "must have required property 'merkleProof'",
-            "must have required property 'content'",
-            "must have required property 'id'",
-            "must have required property 'name'",
-            "must have required property 'description'",
-            "must have required property 'i18n'",
-            "must have required property 'image'",
-            "must have required property 'thumbnail'",
-            "must have required property 'data'",
-            "must have required property 'id'",
-            "must have required property 'name'",
-            "must have required property 'description'",
-            "must have required property 'i18n'",
-            "must have required property 'thumbnail'",
-            "must have required property 'image'",
-            "must have required property 'data'",
-            "either standard XOR thirdparty properties conditions must be met",
-          ]
-        })
+      expect(await deployEntity(components, E1)).toEqual({
+        errors: [
+          'The metadata for this entity type (wearable) is not valid.',
+          "must have required property 'collectionAddress'",
+          "must have required property 'rarity'",
+          'must pass "_isThirdParty" keyword validation',
+          "must have required property 'merkleProof'",
+          "must have required property 'content'",
+          "must have required property 'id'",
+          "must have required property 'name'",
+          "must have required property 'description'",
+          "must have required property 'i18n'",
+          "must have required property 'image'",
+          "must have required property 'thumbnail'",
+          "must have required property 'data'",
+          "must have required property 'id'",
+          "must have required property 'name'",
+          "must have required property 'description'",
+          "must have required property 'i18n'",
+          "must have required property 'thumbnail'",
+          "must have required property 'image'",
+          "must have required property 'data'",
+          'either standard XOR thirdparty properties conditions must be met'
+        ]
+      })
     }
   )
 
   testCaseWithComponents(
-    testEnv,
-    "When wearable metadata is present but incomplete, deployment result should include the proper error",
+    getTestEnv,
+    'When wearable metadata is present but incomplete, deployment result should include the proper error',
     async (components) => {
       makeNoopServerValidator(components)
 
       const identity = createIdentity()
       let E1: EntityCombo = await buildDeployData([identity.address], {
         type: EntityType.WEARABLE,
-        metadata: {
-        },
-        identity,
+        metadata: {},
+        identity
       })
 
-      expect(await deployEntity(components, E1))
-        .toEqual({
-          errors: [
-            "The metadata for this entity type (wearable) is not valid.",
-            "must have required property 'collectionAddress'",
-            "must have required property 'rarity'",
-            "must pass \"_isThirdParty\" keyword validation",
-            "must have required property 'merkleProof'",
-            "must have required property 'content'",
-            "must have required property 'id'",
-            "must have required property 'name'",
-            "must have required property 'description'",
-            "must have required property 'i18n'",
-            "must have required property 'image'",
-            "must have required property 'thumbnail'",
-            "must have required property 'data'",
-            "must have required property 'id'",
-            "must have required property 'name'",
-            "must have required property 'description'",
-            "must have required property 'i18n'",
-            "must have required property 'thumbnail'",
-            "must have required property 'image'",
-            "must have required property 'data'",
-            "either standard XOR thirdparty properties conditions must be met",
-          ]
-        })
+      expect(await deployEntity(components, E1)).toEqual({
+        errors: [
+          'The metadata for this entity type (wearable) is not valid.',
+          "must have required property 'collectionAddress'",
+          "must have required property 'rarity'",
+          'must pass "_isThirdParty" keyword validation',
+          "must have required property 'merkleProof'",
+          "must have required property 'content'",
+          "must have required property 'id'",
+          "must have required property 'name'",
+          "must have required property 'description'",
+          "must have required property 'i18n'",
+          "must have required property 'image'",
+          "must have required property 'thumbnail'",
+          "must have required property 'data'",
+          "must have required property 'id'",
+          "must have required property 'name'",
+          "must have required property 'description'",
+          "must have required property 'i18n'",
+          "must have required property 'thumbnail'",
+          "must have required property 'image'",
+          "must have required property 'data'",
+          'either standard XOR thirdparty properties conditions must be met'
+        ]
+      })
     }
   )
 
   async function deployEntity(
-    components: Pick<AppComponents, "deployer">,
+    components: Pick<AppComponents, 'deployer'>,
     entity: EntityCombo,
     overrideAuditInfo?: Partial<AuditInfo>
   ): Promise<DeploymentResult> {
--- content/test/integration/service/deployments/old-entity-synced.spec.ts
+++ content/test/integration/service/deployments/old-entity-synced.spec.ts
@@ -1,63 +1,71 @@
-import { AuthLinkType, Entity, EntityType } from "@dcl/schemas"
+import { AuthLinkType, Entity, EntityType } from '@dcl/schemas'
 import { createFsComponent } from '../../../../src/ports/fs'
-import { AuditInfo } from "../../../../src/service/deployments/types"
-import { DeploymentContext, DeploymentResult } from "../../../../src/service/Service"
-import { AppComponents } from "../../../../src/types"
-import { makeNoopServerValidator } from "../../../helpers/service/validations/NoOpValidator"
-import { loadStandaloneTestEnvironment, testCaseWithComponents } from "../../E2ETestEnvironment"
-import { EntityCombo } from "../../E2ETestUtils"
+import { AuditInfo } from '../../../../src/service/deployments/types'
+import { DeploymentContext, DeploymentResult } from '../../../../src/service/Service'
+import { AppComponents } from '../../../../src/types'
+import { makeNoopServerValidator } from '../../../helpers/service/validations/NoOpValidator'
+import { setupTestEnvironment, testCaseWithComponents } from '../../E2ETestEnvironment'
+import { EntityCombo } from '../../E2ETestUtils'
 import { getIntegrationResourcePathFor } from '../../resources/get-resource-path'
 
 const fs = createFsComponent()
 
-loadStandaloneTestEnvironment()("Integration - Deployment synced old entity", (testEnv) => {
+describe('Integration - Deployment synced old entity', () => {
+  const getTestEnv = setupTestEnvironment()
 
   testCaseWithComponents(
-    testEnv,
-    "When deploying an entity with cid v0 hashes of entities before ADR 45, it should succeed",
+    getTestEnv,
+    'When deploying an entity with cid v0 hashes of entities before ADR 45, it should succeed',
     async (components) => {
       makeNoopServerValidator(components)
 
       const entity: Entity = {
-        "version": "v3",
-        "id": "QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u",
-        "type": EntityType.PROFILE,
-        "pointers": ["0x4CAb2E350499bAd0eC5941aD6B6995a6a20f0130"],
-        "timestamp": 1632416205936,
-        "content": [
+        version: 'v3',
+        id: 'QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u',
+        type: EntityType.PROFILE,
+        pointers: ['0x4CAb2E350499bAd0eC5941aD6B6995a6a20f0130'],
+        timestamp: 1632416205936,
+        content: [
           {
-            "file": "aFile-zjxd",
-            "hash": "QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF"
+            file: 'aFile-zjxd',
+            hash: 'QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF'
           },
           {
-            "file": "anotherFile-zjxd",
-            "hash": "QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF"
+            file: 'anotherFile-zjxd',
+            hash: 'QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF'
           },
           {
-            "file": "aThirdFile-zjxd",
-            "hash": "QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF"
-          }]
+            file: 'aThirdFile-zjxd',
+            hash: 'QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF'
+          }
+        ]
       }
 
-      const contentFile = await fs.readFile(getIntegrationResourcePathFor('QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF'))
-      const entityFile = await fs.readFile(getIntegrationResourcePathFor('QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u'))
+      const contentFile = await fs.readFile(
+        getIntegrationResourcePathFor('QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF')
+      )
+      const entityFile = await fs.readFile(
+        getIntegrationResourcePathFor('QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u')
+      )
 
       const E1: EntityCombo = {
         deployData: {
           entityId: 'QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u',
           files: new Map([
             ['QmbNcPuVAq6Dv821kfFnyaM59Go3xCeBMnSxYRSHEpwWeF', contentFile],
-            ['QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u', entityFile]]),
+            ['QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u', entityFile]
+          ]),
           authChain: [
             {
-              "type": AuthLinkType.SIGNER,
-              "payload": "0x4CAb2E350499bAd0eC5941aD6B6995a6a20f0130",
-              "signature": ""
+              type: AuthLinkType.SIGNER,
+              payload: '0x4CAb2E350499bAd0eC5941aD6B6995a6a20f0130',
+              signature: ''
             },
             {
-              "type": AuthLinkType.ECDSA_PERSONAL_SIGNED_ENTITY,
-              "payload": "QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u",
-              "signature": "0x10c80e802b3d5faea8e253c809d254ec2e17a4b36c50ade38e0a4e7766c320e903aa262a5be9458802d57f0c69b283855d3734848fc7b68467d0749f082700b01b"
+              type: AuthLinkType.ECDSA_PERSONAL_SIGNED_ENTITY,
+              payload: 'QmYfCVBv7PJCcBh3AutAzNesdGZVKUEd8KCc8Jh1Y8ba4u',
+              signature:
+                '0x10c80e802b3d5faea8e253c809d254ec2e17a4b36c50ade38e0a4e7766c320e903aa262a5be9458802d57f0c69b283855d3734848fc7b68467d0749f082700b01b'
             }
           ]
         },
@@ -71,9 +79,8 @@ loadStandaloneTestEnvironment()("Integration - Deployment synced old entity", (t
     }
   )
 
-
   async function deployEntity(
-    components: Pick<AppComponents, "deployer">,
+    components: Pick<AppComponents, 'deployer'>,
     entity: EntityCombo,
     overrideAuditInfo?: Partial<AuditInfo>
   ): Promise<DeploymentResult> {
