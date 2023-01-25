8d5e11ffd5a3340f93b329e5d69aeaa0af4bf1fc
Mariano Goldman <mariano.goldman@decentraland.org>
2023-01-25 16:23:43 -0300

    encapsulate everything in the script

--- content/src/entrypoints/run-content-files-fixer.ts
+++ content/src/entrypoints/run-content-files-fixer.ts
@@ -1,18 +1,22 @@
-import { Lifecycle } from '@well-known-components/interfaces'
+import { ILoggerComponent, Lifecycle } from '@well-known-components/interfaces'
 import { createLogComponent } from '@well-known-components/logger'
 import path from 'path'
-import { EnvironmentBuilder, EnvironmentConfig } from '../Environment'
+import { Environment, EnvironmentBuilder, EnvironmentConfig } from '../Environment'
 import { createFileSystemContentStorage } from '../ports/contentStorage/fileSystemContentStorage'
 import { createFsComponent } from '../ports/fs'
 import { createDatabaseComponent } from '../ports/postgres'
 import { createConfigComponent } from '@well-known-components/env-config-provider'
-import {
-  ContentFilesFixerComponents,
-  fixMissingProfilesContentFiles
-} from '../service/garbage-collection/FixContentFilesHelper'
 import { createTestMetricsComponent } from '@well-known-components/metrics'
 import { metricsDeclaration } from '../metrics'
 import { createFetchComponent } from '../ports/fetcher'
+import { AppComponents } from '../types'
+import { ContentMapping } from '@dcl/schemas'
+import { saveContentFiles } from '../logic/database-queries/deployments-queries'
+import { ContentStorage } from '../ports/contentStorage/contentStorage'
+import { IFetchComponent } from '@well-known-components/http-server'
+import { Readable } from 'stream'
+
+export type ContentFilesFixerComponents = Pick<AppComponents, 'database' | 'env' | 'fetcher' | 'logs' | 'storage'>
 
 void Lifecycle.run({
   async main(program: Lifecycle.EntryPointParameters<ContentFilesFixerComponents>): Promise<void> {
@@ -42,3 +46,103 @@ void Lifecycle.run({
     return { logs, env, fetcher, database, fs, storage }
   }
 })
+
+export async function fixMissingProfilesContentFiles({
+  database,
+  env,
+  fetcher,
+  logs,
+  storage
+}: ContentFilesFixerComponents) {
+  const logger = logs.getLogger('FixMissingFilesHelper')
+
+  const start = Date.now()
+  logger.info('Fixing missing content files from profiles deployed without references to them')
+
+  try {
+    const result = await database.query(`
+      SELECT *
+      FROM deployments d
+      WHERE entity_type = 'profile'
+      AND NOT EXISTS(
+              SELECT 1
+              FROM content_files cf
+              WHERE cf.deployment = d.id
+          )
+        AND d.deleter_deployment IS NULL
+    `)
+    logger.info(`Found ${result.rowCount} profiles with missing content files.`)
+
+    const regex = /Qm[a-zA-Z0-9]{44}$|^baf[a-zA-Z0-9]{56}/
+    function extract(value: string): string {
+      if (value && value.startsWith('http')) {
+        const matches = value.match(regex)
+        if (matches && matches.length > 0) {
+          return matches[0]
+        }
+      }
+      return value
+    }
+
+    for (const deployment of result.rows) {
+      const contentFiles: ContentMapping[] = deployment['entity_metadata']['v']['avatars']
+        .map((avatar) => {
+          const images: { file: string; hash: string }[] = []
+          if (!avatar.avatar.snapshots) {
+            logger.warn(`Found profile without snapshots: ${JSON.stringify(avatar)}`)
+          } else {
+            for (const [key, value] of Object.entries(avatar.avatar.snapshots || [])) {
+              images.push({ file: `${key}.png`, hash: extract(value as string) })
+            }
+          }
+          return images
+        })
+        .flat()
+
+      try {
+        for (const file of contentFiles) {
+          await ensureFileExistsInStorage(env, logger, storage, fetcher, file.hash)
+        }
+
+        await saveContentFiles(database, deployment.id, contentFiles)
+      } catch (e) {
+        logger.warn(
+          `Error processing deployment id ${deployment.id} for entity id ${
+            deployment.entityId
+          }. ContentFiles: ${JSON.stringify(contentFiles)}`
+        )
+      }
+    }
+  } finally {
+    logger.info(`Fixing missing content files took ${Date.now() - start} ms`)
+  }
+}
+
+async function ensureFileExistsInStorage(
+  env: Environment,
+  logger: ILoggerComponent.ILogger,
+  storage: ContentStorage,
+  fetcher: IFetchComponent,
+  file: string
+): Promise<void> {
+  if (!file) {
+    logger.info(`Invalid file ${file}`)
+    return
+  }
+  if (await storage.exist(file)) {
+    logger.info(`File ${file} already exists`)
+    return
+  }
+
+  logger.info(`Need to download file ${file} from other Catalyst and store it locally`)
+  const url = `https://peer.decentraland.${
+    env.getConfig(EnvironmentConfig.ETH_NETWORK) === 'mainnet' ? 'org' : 'zone'
+  }/content/contents/${file}`
+
+  await fetcher
+    .fetch(url)
+    .then((data) => data.body)
+    .then((stream) => storage.storeStream(file, Readable.from(stream)))
+    .then(() => logger.info(`File ${file} downloaded and stored successfully`))
+    .catch((error: any) => logger.warn(`Problem downloading file ${file}. ${error.message}`))
+}
