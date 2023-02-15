3f3d5b38dee1493800832a75b60e832c67fdded6
Hugo Arregui <hugo@decentraland.org>
2023-02-15 17:15:18 -0300

    update

--- content/src/logic/checker.ts
+++ content/src/logic/checker.ts
@@ -1,18 +1,58 @@
-import {
-  checkerContracts,
-  collectionFactoryContracts,
-  landContracts,
-  registrarContracts,
-  thirdPartyContracts
-} from '@dcl/catalyst-contracts'
 import { L1Checker, L2Checker } from '@dcl/content-validator'
 import { inputCallFormatter, inputBlockNumberFormatter } from './formatters'
 import { RequestManager, HTTPProvider, ContractFactory, toData } from 'eth-connect'
 import { code } from './code'
 
-const commiteesContracts = {
-  polygon: ['0x71d9350Ef44E1e451F00e447C0DfF2d1FB75510a', '0xaeec95a8aa671a6d3fec56594827d7804964fa70'],
-  mumbai: ['0x4bb5ACe5ceB3Dd51ea35fa01a8f9B5507c234270', '0xe18B1361d41afC44658216F3Dc27e48c2336e3c2']
+// TODO: use stateOverride depending on the block
+export const checkerContracts = {
+  goerli: '0xe69DD486AC000186Af573361Fb454Bbbb85AdD85',
+  mainnet: '0x49fd6E40548A67a3FB9cA4fE22ab87885ba10454',
+  mumbai: '0x04149Af3ceDF7a84b37e246a116f0aE4eD429141',
+  polygon: '0xC2D0637FE019817b7B307b9B966E4400EB4aC165'
+}
+
+export const l1Contracts = {
+  goerli: {
+    registrar: '0x6b8da2752827cf926215b43bb8E46Fd7b9dDac35',
+    landContract: '0x25b6B4bac4aDB582a0ABd475439dA6730777Fbf7',
+    stateContract: '0xC9A46712E6913c24d15b46fF12221a79c4e251DC'
+  },
+  mainnet: {
+    registrar: '0x2a187453064356c898cae034eaed119e1663acb8',
+    landContract: '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d',
+    stateContract: '0x959e104e1a4db6317fa58f8295f586e1a978c297'
+  }
+}
+
+const l2Contracts = {
+  mumbai: {
+    thirdParty: '0xEDf516F2D42A47F9cE0B145Fe0dbB76975379889',
+    factories: [
+      { address: '0x2A72Ec4241Ac4fBc915ae98aC5a5b01AdE721f4B', sinceBlock: 14517381 },
+      { address: '0xDDb3781Fff645325C8896AA1F067bAa381607ecc', sinceBlock: 26012021 }
+    ],
+    commitees: [
+      { address: '0x4bb5ACe5ceB3Dd51ea35fa01a8f9B5507c234270', sinceBlock: 14517376 },
+      { address: '0xe18B1361d41afC44658216F3Dc27e48c2336e3c2', sinceBlock: 18881998 }
+    ]
+  },
+  polygon: {
+    thirdParty: '0x1C436C1EFb4608dFfDC8bace99d2B03c314f3348',
+    factories: [
+      {
+        address: '0xB549B2442b2BD0a53795BC5cDcBFE0cAF7ACA9f8',
+        sinceBlock: 15202563
+      },
+      {
+        address: '0x3195e88aE10704b359764CB38e429D24f1c2f781',
+        sinceBlock: 28121692
+      }
+    ],
+    commitees: [
+      { address: '0x71d9350Ef44E1e451F00e447C0DfF2d1FB75510a', sinceBlock: 15202559 },
+      { address: '0xaeec95a8aa671a6d3fec56594827d7804964fa70', sinceBlock: 19585299 }
+    ]
+  }
 }
 
 const checkerAbi = [
@@ -180,8 +220,7 @@ const checkerAbi = [
 
 export async function createL1Checker(provider: HTTPProvider, network: 'mainnet' | 'goerli'): Promise<L1Checker> {
   const checkerAddress = checkerContracts[network]
-  const { landContractAddress, stateContractAddress } = landContracts[network]
-  const registrar = registrarContracts[network]
+  const contracts = l1Contracts[network]
 
   const requestManager = new RequestManager(provider)
   const factory = new ContractFactory(requestManager, checkerAbi)
@@ -192,7 +231,6 @@ export async function createL1Checker(provider: HTTPProvider, network: 'mainnet'
   }
 
   async function callMulticallCheckerMethod(args: any, block: number | string) {
-    // TODO: use stateOverride depending on the block
     const payload = await checker.multicall.toPayload(args)
     payload.to = checkerAddress
     const call = {
@@ -208,7 +246,7 @@ export async function createL1Checker(provider: HTTPProvider, network: 'mainnet'
     async checkLAND(ethAddress: string, parcels: [number, number][], block: number): Promise<boolean[]> {
       const multicallPayload = await Promise.all(
         parcels.map(async ([x, y]) => {
-          const payload = checker.checkLAND.toPayload(ethAddress, landContractAddress, stateContractAddress, x, y)
+          const payload = checker.checkLAND.toPayload(ethAddress, contracts.landContract, contracts.stateContract, x, y)
           return payload.data
         })
       )
@@ -218,7 +256,7 @@ export async function createL1Checker(provider: HTTPProvider, network: 'mainnet'
     async checkNames(ethAddress: string, names: string[], block: number): Promise<boolean[]> {
       const multicallPayload = await Promise.all(
         names.map(async (name) => {
-          const payload = await checker.checkName.toPayload(ethAddress, registrar, name)
+          const payload = await checker.checkName.toPayload(ethAddress, contracts.registrar, name)
           return payload.data
         })
       )
@@ -233,22 +271,29 @@ export async function createL2Checker(provider: HTTPProvider, network: 'mumbai' 
   const requestManager = new RequestManager(provider)
   const factory = new ContractFactory(requestManager, checkerAbi)
   const checker = (await factory.at(checkerAddress)) as any
-  const commitees = commiteesContracts[network]
 
   const stateOverride = {
     [checkerAddress]: { code }
   }
 
-  const registry = thirdPartyContracts[network]
-  const { v2, v3 } = collectionFactoryContracts[network]
+  const contracts = l2Contracts[network]
 
-  const factories = [v2, v3]
+  async function callMulticallCheckerMethod(args: any, block: number | string) {
+    const payload = await checker.multicall.toPayload(args)
+    payload.to = checkerAddress
+    const call = {
+      method: 'eth_call',
+      params: [inputCallFormatter(payload), inputBlockNumberFormatter(block), stateOverride]
+    }
+
+    const data = toData(await requestManager.sendAsync(call))
+    return checker.multicall.unpackOutput(data)
+  }
 
   async function callCheckerMethod(method: any, args: any[], block: number | string) {
     const payload = await method.toPayload(...args)
     payload.to = checkerAddress
 
-    // TODO: use stateOverride depending on the block
     const call = {
       method: 'eth_call',
       params: [inputCallFormatter(payload), inputBlockNumberFormatter(block), stateOverride]
@@ -263,17 +308,34 @@ export async function createL2Checker(provider: HTTPProvider, network: 'mumbai' 
       ethAddress: string,
       contractAddress: string,
       assetId: string,
-      hash: string,
+      hashes: string[],
       block: number
     ): Promise<boolean> {
-      return callCheckerMethod(
-        checker.validateWearables,
-        [ethAddress, factories, contractAddress, assetId, hash, commitees],
-        block
+      const factories = contracts.factories
+        .filter(({ sinceBlock }) => block >= sinceBlock)
+        .map(({ address }) => address)
+      const commitees = contracts.commitees
+        .filter(({ sinceBlock }) => block >= sinceBlock)
+        .map(({ address }) => address)
+      const multicallPayload = await Promise.all(
+        hashes.map(async (hash) => {
+          const payload = checker.validateWearables.toPayload(
+            ethAddress,
+            factories,
+            contractAddress,
+            assetId,
+            hash,
+            commitees
+          )
+          return payload.data
+        })
       )
+
+      const result = (await callMulticallCheckerMethod(multicallPayload, block)) as boolean[]
+      return result.some((r) => r)
     },
-    async validateThirdParty(_ethAddress: string, tpId: string, root: Buffer, block: number): Promise<boolean> {
-      return callCheckerMethod(checker.validateThirdParty, [registry, tpId, root], block)
+    async validateThirdParty(tpId: string, root: Buffer, block: number): Promise<boolean> {
+      return callCheckerMethod(checker.validateThirdParty, [contracts.thirdParty, tpId, root], block)
     }
   }
 }
