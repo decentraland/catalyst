bbf854c03f27ccf9546e8916edbb736cf8f24d40
Hugo Arregui <hugo@decentraland.org>
2023-05-05 16:33:16 -0300

    Save Fri May  5 04:33:16 PM -03 2023

--- content/jest.globalTeardown.ts
+++ content/jest.globalTeardown.ts
@@ -1,9 +1,4 @@
-import printApiCoverage from './printApiCoverage'
-
 const globalTeardown = async (): Promise<void> => {
-  if (process.env.API_COVERAGE === 'true') {
-    printApiCoverage()
-  }
   await globalThis.__POSTGRES_CONTAINER__?.stop()
 }
 
