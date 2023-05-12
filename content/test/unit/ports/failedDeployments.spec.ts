1b9c85ee82106a02992f3d98263119a566fb81a4
Hugo Arregui <hugo@decentraland.org>
2023-05-12 17:54:34 -0300

    Save Fri May 12 05:54:34 PM -03 2023

--- content/test/unit/ports/failedDeployments.spec.ts
+++ content/test/unit/ports/failedDeployments.spec.ts
@@ -3,7 +3,7 @@ import { createTestMetricsComponent } from '@well-known-components/metrics'
 import * as failedDeploymentQueries from '../../../src/logic/database-queries/failed-deployments-queries'
 import { metricsDeclaration } from '../../../src/metrics'
 import { createFailedDeployments, FailureReason, SnapshotFailedDeployment } from '../../../src/ports/failedDeployments'
-import { createTestDatabaseComponent } from '../../../src/ports/postgres'
+import { createTestDatabaseComponent, DatabaseTransactionalClient } from '../../../src/ports/postgres'
 import { AppComponents } from '../../../src/types'
 
 describe('failed deployments', () => {
@@ -70,9 +70,9 @@ describe('failed deployments', () => {
   it('should report failed deployment and delete the previous one if there was one with the same entity id', async () => {
     const saveSpy = jest.spyOn(failedDeploymentQueries, 'saveSnapshotFailedDeployment').mockImplementation()
     const deleteSpy = jest.spyOn(failedDeploymentQueries, 'deleteFailedDeployment').mockImplementation()
-    const txSpy = jest
-      .spyOn(database, 'transaction')
-      .mockImplementation(async (fnToRun) => await fnToRun({ insideTx: true, ...database }))
+
+    const txDb: DatabaseTransactionalClient = { insideTx: true, ...database }
+    const txSpy = jest.spyOn(database, 'transaction').mockImplementation((fnToRun) => fnToRun(txDb))
     const components = { metrics, database }
     const failedDeployments = await createAndStartFailedDeploymentsWith(components, [aFailedDeployment])
     const newFailedDeploymentWithSameId = {
@@ -86,7 +86,7 @@ describe('failed deployments', () => {
     expect(failed).toHaveLength(1)
     expect(failed[0]).toEqual(newFailedDeploymentWithSameId)
     expect(failed[0].failureTimestamp).toEqual(aFailedDeployment.failureTimestamp + 10)
-    expect(saveSpy).toHaveBeenCalledWith(database, newFailedDeploymentWithSameId)
+    expect(saveSpy).toHaveBeenCalledWith(txDb, newFailedDeploymentWithSameId)
     expect(deleteSpy).toHaveBeenCalledWith(database, aFailedDeployment.entityId)
     expect(txSpy).toHaveBeenCalled()
   })
