d61957ee882189a497ab03bbed64461d95bc7b38
Hugo Arregui <hugo@decentraland.org>
2023-06-02 16:20:39 -0300

    Save Fri Jun  2 04:20:39 PM -03 2023

--- content/src/controller/handlers/create-entity-handler.ts
+++ content/src/controller/handlers/create-entity-handler.ts
@@ -2,7 +2,6 @@ import { PostEntity200, PostEntity400 } from '@dcl/catalyst-api-specs/lib/client
 import { Field } from '@well-known-components/multipart-wrapper'
 import { AuthChain, Authenticator, AuthLink, EthAddress, Signature } from '@dcl/crypto'
 import { DeploymentContext, isInvalidDeployment, isSuccessfulDeployment } from '../../deployment-types'
-import { CURRENT_CONTENT_VERSION } from '../../Environment'
 import { FormHandlerContextWithPath, InvalidRequestError } from '../../types'
 
 type ContentFile = {
