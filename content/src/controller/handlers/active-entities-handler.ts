0d78ea5702e1a9b64d9aaccccec76e568b59feed
Hugo Arregui <hugo@decentraland.org>
2023-07-17 14:44:30 -0300

    replace ajv with joi

--- content/src/controller/handlers/active-entities-handler.ts
+++ content/src/controller/handlers/active-entities-handler.ts
@@ -1,39 +1,15 @@
 import { Entity } from '@dcl/catalyst-api-specs/lib/client'
 import { HandlerContextWithPath, InvalidRequestError } from '../../types'
-import { JSONSchemaType } from 'ajv'
-import { ajv } from '../../logic/ajv'
+import Joi from 'joi'
 
-type Body = { ids: string[] } | { pointers: string[] }
-
-const schema: JSONSchemaType<Body> = {
-  oneOf: [
-    {
-      type: 'object',
-      properties: {
-        ids: {
-          type: 'array',
-          items: { type: 'string' },
-          minItems: 1
-        }
-      },
-      required: ['ids']
-    },
-    {
-      type: 'object',
-      properties: {
-        pointers: {
-          type: 'array',
-          items: { type: 'string' },
-          minItems: 1
-        }
-      },
-      required: ['pointers']
-    }
-  ],
-  errorMessage: 'ids or pointers must be present, but not both'
-}
-
-const validateBody = ajv.compile(schema)
+const schema = Joi.alternatives().try(
+  Joi.object({
+    ids: Joi.array().items(Joi.string()).min(1).required()
+  }),
+  Joi.object({
+    pointers: Joi.array().items(Joi.string()).min(1).required()
+  })
+)
 
 // Method: POST
 // Body: { ids: string[], pointers: string[]}
@@ -41,18 +17,16 @@ export async function getActiveEntitiesHandler(
   context: HandlerContextWithPath<'database' | 'activeEntities' | 'denylist', '/entities/active'>
 ): Promise<{ status: 200; body: Entity[] }> {
   const { database, activeEntities, denylist } = context.components
-  const body = await context.request.json()
-  const valid = validateBody(body)
-  if (!valid) {
-    const error = validateBody.errors!.map((e) => e.message).join(', ')
-    throw new InvalidRequestError(error)
+  const { error, value: body } = schema.validate(await context.request.json())
+
+  if (error) {
+    throw new InvalidRequestError('ids or pointers must be present, but not both')
   }
 
-  const ids = (body as any).ids
-  const pointers = (body as any).pointers
-
   const entities: Entity[] = (
-    ids ? await activeEntities.withIds(database, ids) : await activeEntities.withPointers(database, pointers)
+    body.ids
+      ? await activeEntities.withIds(database, body.ids)
+      : await activeEntities.withPointers(database, body.pointers)
   ).filter((result) => !denylist.isDenylisted(result.id))
 
   return {
