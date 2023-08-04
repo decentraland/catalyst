1190a19ace14a8d8fabae76618b448ab64a76221
Hugo Arregui <hugo@decentraland.org>
2023-08-04 15:58:07 -0300

    esm

--- content/vitest.config.ts
+++ content/vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    globalSetup: ['./jest.globalSetup.ts']
+    globalSetup: ['./jest.globalSetup.ts'],
+    typecheck: {
+      tsconfig: './test/tsconfig.json'
+    }
   }
 })
