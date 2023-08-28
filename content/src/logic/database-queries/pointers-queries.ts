5806c9ce33177b0edbf1db2975ca6fb00cd0a1c5
Mariano Goldman <mariano.goldman@decentraland.org>
2023-08-28 14:01:57 -0300

    fix typo

--- content/src/logic/database-queries/pointers-queries.ts
+++ content/src/logic/database-queries/pointers-queries.ts
@@ -5,7 +5,7 @@ export async function gerUrnsThatMatchCollectionUrnPrefix(
   database: DatabaseClient,
   collectionUrn: string
 ): Promise<string[]> {
-  // sql-template-strings doesn't allow ' in the query string
+  // sql-template-strings don't allow ' in the query string
   const matchingString = `${collectionUrn}%`
   const query = SQL`SELECT pointer FROM active_pointers as p WHERE p.pointer LIKE ${matchingString} ORDER BY pointer DESC;`
 
