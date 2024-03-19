e2974a563921156cc8b4618a652c1cdf6f8d0824
Hugo Arregui <969314+hugoArregui@users.noreply.github.com>
2024-03-19 16:49:14 -0300

    chore: bump WKC, schemas and content-validator (#1759)

--- content/src/metrics.ts
+++ content/src/metrics.ts
@@ -2,7 +2,7 @@ import { metricsDefinitions as snapshotFetcherMetricsDefinitions } from '@dcl/sn
 import { metricsDefinitions as blockIndexerMetricsDefinitions } from '@dcl/block-indexer'
 import { metricDeclarations as loggerMetricDeclarations } from '@well-known-components/logger'
 import { validateMetricsDeclaration } from '@well-known-components/metrics'
-import { getDefaultHttpMetrics } from '@well-known-components/metrics/dist/http'
+import { getDefaultHttpMetrics } from '@well-known-components/http-server'
 import { metricDeclarations as theGraphMetricDeclarations } from '@well-known-components/thegraph-component'
 import { sequentialJobMetrics } from './ports/sequecuentialTaskExecutor'
 
