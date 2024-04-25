51e604b245aa0fb5173298eb3273511abd8fd504
Hugo Arregui <hugo@decentraland.org>
2024-04-25 10:08:49 -0300

    Save Thu Apr 25 10:08:49 AM -03 2024

--- content/src/service/synchronization/retryFailedDeployments.ts
+++ content/src/service/synchronization/retryFailedDeployments.ts
@@ -27,8 +27,8 @@ export const createRetryFailedDeployments = (
 ): IRetryFailedDeploymentsComponent => {
   const retryDelay = components.env.getConfig<number>(EnvironmentConfig.RETRY_FAILED_DEPLOYMENTS_DELAY_TIME)
   const logger = components.logs.getLogger('RetryFailedDeployments')
-  let ac: AbortController | undefined = new AbortController()
-  const signal = ac.signal
+  // let ac: AbortController | undefined = new AbortController()
+  // const signal = ac.signal
 
   let running = false
   return {
@@ -38,15 +38,15 @@ export const createRetryFailedDeployments = (
     },
     stop: async () => {
       running = false
-      if (ac) {
-        ac.abort()
-      }
-      ac = undefined
+      // if (ac) {
+      //   ac.abort()
+      // }
+      // ac = undefined
       logger.debug('Stopping retry failed deployments')
     },
     schedule: async () => {
       while (running) {
-        await setTimeout(retryDelay, null, { signal })
+        await setTimeout(retryDelay, null, {})
         if (!running) {
           return
         }
